488153119eba0dea4169f6d31b43bbaf124c6705
hammingweight <hammingweight@cc4b8374-00f8-dcee-eb2c-e192c0053825>
2011-02-05 10:34:18 +0000

    Added iso8583_bit_field module.

---
+++ trunk/src/iso8583_bit_field.erl
@@ -0,0 +1,25 @@
+%% Author: carl
+%% Created: 05 Feb 2011
+%% Description: TODO: Add description to iso8583_bit_field
+-module(iso8583_bit_field).
+
+%%
+%% Include files
+%%
+
+%%
+%% Exported Functions
+%%
+-export([new/1]).
+
+%%
+%% API Functions
+%%
+new(_NumFields) ->
+	{iso8583_bit_field, ok}.
+
+
+%%
+%% Local Functions
+%%
+
