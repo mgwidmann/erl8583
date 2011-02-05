29165331639d6bcfe3ea7d12c3a59f2140f81c4e
hammingweight <hammingweight@cc4b8374-00f8-dcee-eb2c-e192c0053825>
2011-02-05 15:28:34 +0000

    Simplified the iso8583_bit_map module.

--- src/iso8583_bit_map.erl
+++ src/iso8583_bit_map.erl
@@ -10,27 +10,35 @@
 %%
 %% Exported Functions
 %%
--export([new/1, set/3, get/2]).
+-export([new/0, set/3, get/2, get_indexes/1, to_list/1]).
 
 %%
 %% API Functions
 %%
-new(NumFields) ->
-	Dict = dict:new(),
-	UpdatedDict = dict:store(max_index, NumFields, Dict),
-	{iso8583_bit_map, UpdatedDict}.
+new() ->
+	{iso8583_bit_map, dict:new()}.
 
 set(Index, Value, BitMap) when Index >= 0 ->
 	{iso8583_bit_map, Dict} = BitMap,
-	MaxIndex = dict:fetch(max_index, Dict),
-	case Index =< MaxIndex andalso not dict:is_key(Index, Dict) of
-		true ->
+	case dict:is_key(Index, Dict) of
+		false ->
 			{iso8583_bit_map, dict:store(Index, Value, Dict)}
 	end.
 
 get(Index, BitMap) when is_integer(Index) ->
 	{iso8583_bit_map, Dict} = BitMap,
 	dict:fetch(Index, Dict).
+
+get_indexes(BitMap) ->
+	{iso8583_bit_map, Dict} = BitMap,
+	KeyValues = dict:to_list(Dict),
+	Keys = [K || {K, _V} <- KeyValues],
+	lists:sort(Keys).
+
+to_list(BitMap) ->
+	{iso8583_bit_map, Dict} = BitMap,
+	Keys = get_indexes(BitMap),
+	[{K, V} || K <- Keys, V <- dict:fetch(K, Dict)].
 
 %%
 %% Local Functions
--- src/test_iso8583_bit_map.erl
+++ src/test_iso8583_bit_map.erl
@@ -17,34 +17,50 @@
 %% API Functions
 %%
 new_test() ->
-	{iso8583_bit_map, _} = iso8583_bit_map:new(10).
+	{iso8583_bit_map, _} = iso8583_bit_map:new().
 								
 set_test() ->
-	BitMap = iso8583_bit_map:new(65),
+	BitMap = iso8583_bit_map:new(),
 	{iso8583_bit_map, _} = iso8583_bit_map:set(0, "0200", BitMap),
 	{iso8583_bit_map, _} = iso8583_bit_map:set(65, "hello", BitMap).
 
 set_field_negative_test() ->
-	BitMap = iso8583_bit_map:new(64),
+	BitMap = iso8583_bit_map:new(),
 	?assertError(_, iso8583_bit_map:set(-1, "0200", BitMap)).
 	
-set_field_index_too_big_test() ->
-	BitMap = iso8583_bit_map:new(64),
-	?assertError(_, iso8583_bit_map:set(65, "hello", BitMap)).
-
 set_field_index_twice_test() ->
-	BitMap = iso8583_bit_map:new(64),
+	BitMap = iso8583_bit_map:new(),
 	UpdatedBitMap = iso8583_bit_map:set(64, "hello", BitMap),
 	?assertError(_, iso8583_bit_map:set(64, "hello", UpdatedBitMap)).
 	
 get_test() ->
-	BitMap = iso8583_bit_map:new(64),
+	BitMap = iso8583_bit_map:new(),
 	UpdatedBitMap = iso8583_bit_map:set(64, "hello", BitMap),
 	"hello" = iso8583_bit_map:get(64, UpdatedBitMap).
 
 get_no_value_test() ->
-	BitMap = iso8583_bit_map:new(64),
-	?assertError(_, iso8583_bit_map:get(64, BitMap)).	
+	BitMap = iso8583_bit_map:new(),
+	?assertError(_, iso8583_bit_map:get(64, BitMap)).
+
+get_indexes_empty_test() ->
+	BitMap = iso8583_bit_map:new(),
+	[] = iso8583_bit_map:get_indexes(BitMap).
+
+get_indexes_test() ->
+	BitMap1 = iso8583_bit_map:new(),
+	BitMap2 = iso8583_bit_map:set(64, "hello", BitMap1),
+	BitMap3 = iso8583_bit_map:set(0, "foo", BitMap2),
+	BitMap4 = iso8583_bit_map:set(65, "bar", BitMap3),
+	[0, 64, 65] = iso8583_bit_map:get_indexes(BitMap4).
+
+to_list_test() ->
+	BitMap1 = iso8583_bit_map:new(),
+	BitMap2 = iso8583_bit_map:set(64, "hello", BitMap1),
+	BitMap3 = iso8583_bit_map:set(0, "foo", BitMap2),
+	BitMap4 = iso8583_bit_map:set(65, "bar", BitMap3),
+	%[{0, "foo"}, {64, "hello"}, {65, "bar"}] = 
+	iso8583_bit_map:to_list(BitMap4).
+
 %%
 %% Local Functions
 %%
