a417c3a96b5408dd72d72f1d4b994011a7a9860b
hammingweight <hammingweight@cc4b8374-00f8-dcee-eb2c-e192c0053825>
2011-02-06 18:48:30 +0000

    xml_unmarshaller should support nested fields.

--- trunk/src/test_xml_unmarshaller.erl
+++ trunk/src/test_xml_unmarshaller.erl
@@ -11,7 +11,7 @@
 %%
 %% Exported Functions
 %%
--export([]).
+-export([foo/0]).
 
 %%
 %% API Functions
@@ -35,6 +35,30 @@ xml_unmarshall_with_text_test() ->
 	?assertEqual("0800", iso8583_message:get(0, Message)),
 	?assertEqual("333333", iso8583_message:get(3, Message)),
 	?assertEqual([0, 3], iso8583_message:get_fields(Message)).
+	
+xml_unmarshall_complex_test() ->
+	XmlMessage = "<isomsg>" ++
+      "<field id=\"0\" value=\"0810\"/>" ++
+      "<field id=\"3\" value=\"333333\"/>" ++
+      "<field id=\"39\" value=\"00\"/>" ++
+      "<isomsg id=\"48\">" ++
+        "<field id=\"1\" value=\"hello\"/>" ++
+      "</isomsg>" ++
+    "</isomsg>",
+	Message = xml_unmarshaller:unmarshall(XmlMessage),
+	?assertEqual("0810", iso8583_message:get(0, Message)),
+	?assertEqual("333333", iso8583_message:get(3, Message)),
+	?assertEqual([0, 3, 39, 48], iso8583_message:get_fields(Message)).
+
+foo() ->
+	{ok, Sock} = gen_tcp:connect("localhost", 8000, [list, {packet, 0}, {active, true}]),
+	IsoMsg1 = iso8583_message:new(),
+	IsoMsg2 = iso8583_message:set(0, "0800", IsoMsg1),
+	IsoMsg3 = iso8583_message:set(3, "333333", IsoMsg2),
+	ok = gen_tcp:send(Sock, xml_marshaller:marshall(IsoMsg3)),
+	receive {tcp, _, IsoResp} -> IsoResp end,
+	IsoResp1 = xml_unmarshaller:unmarshall(IsoResp),
+	iso8583_message:get(39, IsoResp1).
 	
 
 %%
