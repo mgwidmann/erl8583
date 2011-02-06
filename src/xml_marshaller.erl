df1c27976a3ade389aae67ec5962ec112dfb7dcf
hammingweight <hammingweight@cc4b8374-00f8-dcee-eb2c-e192c0053825>
2011-02-06 05:03:26 +0000

    Minor refactorings. Started work on xml_marshaller module.

---
+++ src/xml_marshaller.erl
@@ -0,0 +1,25 @@
+%% Author: carl
+%% Created: 06 Feb 2011
+%% Description: TODO: Add description to xml_marshaller
+-module(xml_marshaller).
+
+%%
+%% Include files
+%%
+
+%%
+%% Exported Functions
+%%
+-export([marshall/1]).
+
+%%
+%% API Functions
+%%
+marshall(_IsoMsg) ->
+	"<isomsg/>".
+
+
+%%
+%% Local Functions
+%%
+
