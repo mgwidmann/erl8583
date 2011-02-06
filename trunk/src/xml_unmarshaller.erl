6176c53e87bbfcab58a7bdb37ce3e197ad2a0ffe
hammingweight <hammingweight@cc4b8374-00f8-dcee-eb2c-e192c0053825>
2011-02-06 20:22:47 +0000

    Refactored.

--- trunk/src/xml_unmarshaller.erl
+++ trunk/src/xml_unmarshaller.erl
@@ -21,15 +21,15 @@ unmarshall(XmlMessage) ->
 	{Xml, []} = xmerl_scan:string(XmlMessage),
 	isomsg = Xml#xmlElement.name,
 	ChildNodes = Xml#xmlElement.content,
-	unmarshall(ChildNodes, iso8583_message:new()).
+	unmarshall(ChildNodes, iso8583_message:new(), iso8583_message).
 
 
 %%
 %% Local Functions
 %%
-unmarshall([], Iso8583Msg) ->
+unmarshall([], Iso8583Msg, _Module) ->
 	Iso8583Msg;
-unmarshall([Field|T], Iso8583Msg) when is_record(Field, xmlElement) ->
+unmarshall([Field|T], Iso8583Msg, Module) when is_record(Field, xmlElement) ->
 	case Field#xmlElement.name of
 		field ->
 			Attributes = Field#xmlElement.attributes,
@@ -49,38 +49,10 @@ unmarshall([Field|T], Iso8583Msg) when is_record(Field, xmlElement) ->
 			id = Attr1#xmlAttribute.name,
 			Id = Attr1#xmlAttribute.value,
 			ChildNodes = Field#xmlElement.content,
-			Value = unmarshall_bit_map(ChildNodes, iso8583_bit_map:new())
+			Value = unmarshall(ChildNodes, iso8583_bit_map:new(), iso8583_bit_map)
 	end,	
-	UpdatedMsg = iso8583_message:set(list_to_integer(Id), Value, Iso8583Msg),
-	unmarshall(T, UpdatedMsg);
-unmarshall([_H|T], Iso8583Msg) ->
-	unmarshall(T, Iso8583Msg).
+	UpdatedMsg = Module:set(list_to_integer(Id), Value, Iso8583Msg),
+	unmarshall(T, UpdatedMsg, Module);
+unmarshall([_H|T], Iso8583Msg, Module) ->
+	unmarshall(T, Iso8583Msg, Module).
 
-unmarshall_bit_map([], BitMap) ->
-	BitMap;
-unmarshall_bit_map([Field|T], BitMap) when is_record(Field, xmlElement) ->
-	case Field#xmlElement.name of
-		field ->
-			Attributes = Field#xmlElement.attributes,
-			[Attr1, Attr2] = Attributes,
-			case Attr1#xmlAttribute.name of
-				id ->
-					Id = Attr1#xmlAttribute.value,
-					value = Attr2#xmlAttribute.name,
-					Value = Attr2#xmlAttribute.value;
-				value ->
-					Id = Attr2#xmlAttribute.value,
-					value = Attr1#xmlAttribute.name,
-					Value = Attr1#xmlAttribute.value
-			end;
-		isomsg ->
-			[Attr1] = Field#xmlElement.attributes,
-			id = Attr1#xmlAttribute.name,
-			Id = Attr1#xmlAttribute.value,
-			ChildNodes = Field#xmlElement.content,
-			Value = unmarshall_bit_map(ChildNodes, iso8583_bit_map:new())
-	end,	
-	UpdatedMsg = iso8583_bit_map:set(list_to_integer(Id), Value, BitMap),
-	unmarshall(T, UpdatedMsg);
-unmarshall_bit_map([_H|T], BitMap) ->
-	unmarshall(T, BitMap).
