9770a6fb500577eb78da2d206c15e538fe1360bf
hammingweight <hammingweight@cc4b8374-00f8-dcee-eb2c-e192c0053825>
2011-02-13 11:09:07 +0000

    LLVAR fields can be unmarshalled.

--- src/ascii_unmarshaller.erl
+++ src/ascii_unmarshaller.erl
@@ -19,12 +19,46 @@ unmarshall(Msg) ->
 	unmarshall(Msg, iso8583_fields).
 
 unmarshall(Msg, _EncodingRules) ->
-	IsoMsg = iso8583_message:new(),
-	iso8583_message:set(0, Msg, IsoMsg).
-
+	IsoMsg1 = iso8583_message:new(),
+	{Mti, Rest} = lists:split(4, Msg),
+	IsoMsg2 = iso8583_message:set(0, Mti, IsoMsg1),
+	{FieldIds, Fields} = extract_fields(Rest),
+	decode_fields(FieldIds, Fields, IsoMsg2).
 
 
 %%
 %% Local Functions
 %%
+extract_fields([]) ->
+	{[], []};
+extract_fields(Message) ->
+	{AsciiBitMap, Fields} = lists:split(16, Message),
+	BitMap = string_utils:ascii_hex_to_string(AsciiBitMap),
+	extract_fields(BitMap, 0, 8, {[], Fields}).
 
+extract_fields([], _Offset, _Index, {FieldIds, Fields}) ->
+	{FieldIds, Fields};
+extract_fields([_Head|Tail], Offset, 0, {FieldIds, Fields}) ->
+	extract_fields(Tail, Offset+1, 8, {FieldIds, Fields});
+extract_fields([Head|Tail], Offset, Index, {FieldIds, Fields}) ->
+	case Head band (1 bsl (Index-1)) of
+		0 ->
+			extract_fields([Head|Tail], Offset, Index-1, {FieldIds, Fields});
+		_ ->
+			extract_fields([Head|Tail], Offset, Index-1, {[Offset*8+9-Index|FieldIds], Fields})
+	end.
+			
+decode_fields([], _, Result) ->
+	Result;
+decode_fields([Field|Tail], Fields, Result) ->
+	Encoding = iso8583_fields:get_encoding(Field),
+	{Value, UpdatedFields} = decode_field(Encoding, Fields),
+	UpdatedResult = iso8583_message:set(Field, Value, Result),
+	decode_fields(Tail, UpdatedFields, UpdatedResult).
+	
+decode_field({n, llvar, _}, Fields) ->
+	{N, Rest} = lists:split(2, Fields),
+	Length = list_to_integer(N),
+	{Value, RemainingFields} = lists:split(Length, Rest),
+	{Value, RemainingFields}.
+	
--- src/test_ascii_unmarshaller.erl
+++ src/test_ascii_unmarshaller.erl
@@ -27,3 +27,9 @@ unmarshall_mti_test() ->
 	"0210" = iso8583_message:get(0, Msg),
 	[0] = iso8583_message:get_fields(Msg).
 
+pan_test() ->
+	Msg = ascii_unmarshaller:unmarshall("02004000000000000000165234567890123456"),
+	"0200" = iso8583_message:get(0, Msg),
+	[0, 2] = iso8583_message:get_fields(Msg),
+	"5234567890123456" = iso8583_message:get(2, Msg).
+	
