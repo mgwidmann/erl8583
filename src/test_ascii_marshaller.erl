6f72d5d650588951bb013deb64d7f8b60e5adee2
hammingweight <hammingweight@cc4b8374-00f8-dcee-eb2c-e192c0053825>
2011-02-13 09:37:55 +0000

    fields 8, 9, 10 can be ASCII marshalled.

--- src/test_ascii_marshaller.erl
+++ src/test_ascii_marshaller.erl
@@ -56,6 +56,15 @@ fields_5_6_7_test() ->
 	Msg4 = iso8583_message:set(6, "2", Msg3),	
 	Msg5 = iso8583_message:set(7, "0131081200", Msg4),	
 	"02000E000000000000000000000000010000000000020131081200" = ascii_marshaller:marshall(Msg5).
+
+fields_8_9_10_test() ->
+	Msg1 = iso8583_message:new(),
+	Msg2 = iso8583_message:set(0, "1300", Msg1),
+	Msg3 = iso8583_message:set(8, "1", Msg2),	
+	Msg4 = iso8583_message:set(9, "2", Msg3),	
+	Msg5 = iso8583_message:set(10, "3", Msg4),	
+	"130001C0000000000000000000010000000200000003" = ascii_marshaller:marshall(Msg5).
+	
 %%
 %% Local Functions
 %%
