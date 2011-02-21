8ae5eb5c28ad19b441303febcf69ef145637ec2c
hammingweight <hammingweight@cc4b8374-00f8-dcee-eb2c-e192c0053825>
2011-02-21 17:21:36 +0000

    Can extract fields from bit map for bit maps of 64 bit length.

--- trunk/src/binary_unmarshaller.erl
+++ trunk/src/binary_unmarshaller.erl
@@ -20,13 +20,43 @@ unmarshall(Msg) ->
 
 unmarshall(Msg, _EncodingRules) ->
 	IsoMsg1 = iso8583_message:new(),
-	{MtiBin, _} = split_binary(Msg, 2),
+	{MtiBin, Rest} = split_binary(Msg, 2),
 	Mti = convert:binary_to_ascii_hex(MtiBin),
-	iso8583_message:set(0, Mti, IsoMsg1).
-
+	IsoMsg2 = iso8583_message:set(0, Mti, IsoMsg1),
+	{FieldIds, _Fields} = extract_fields(Rest),
+	populate_fields(FieldIds, IsoMsg2).
+	
 
 
 %%
 %% Local Functions
 %%
+extract_fields(<<>>) ->
+	{[], <<>>};
+extract_fields(Message) ->
+	BitMapLength = get_bit_map_length(Message),
+	{BinaryBitMap, Fields} = split_binary(Message, BitMapLength),
+	BitMap = binary_to_list(BinaryBitMap),
+	extract_fields(BitMap, 0, 8, {[], Fields}).
 
+get_bit_map_length(_Message) ->
+	8.
+
+extract_fields([], _Offset, _Index, {FieldIds, Fields}) ->
+	Ids = lists:sort(FieldIds),
+	{[Id || Id <- Ids, Id rem 64 =/= 1], Fields};
+extract_fields([_Head|Tail], Offset, 0, {FieldIds, Fields}) ->
+	extract_fields(Tail, Offset+1, 8, {FieldIds, Fields});
+extract_fields([Head|Tail], Offset, Index, {FieldIds, Fields}) ->
+	case Head band (1 bsl (Index-1)) of
+		0 ->
+			extract_fields([Head|Tail], Offset, Index-1, {FieldIds, Fields});
+		_ ->
+			extract_fields([Head|Tail], Offset, Index-1, {[Offset*8+9-Index|FieldIds], Fields})
+	end.
+
+populate_fields([], Msg) ->
+	Msg;
+populate_fields([Id|Tail], Msg) ->
+	UpdatedMsg = iso8583_message:set(Id, "Value", Msg),
+	populate_fields(Tail, UpdatedMsg).
--- trunk/src/test_binary_unmarshaller.erl
+++ trunk/src/test_binary_unmarshaller.erl
@@ -30,5 +30,11 @@ unmarshall_mti_test() ->
 	Msg2 = binary_unmarshaller:unmarshall(<<2, 0>>),
 	"0200" = iso8583_message:get(0, Msg2).
 	
+field_2_test() ->
+	Msg1 = binary_unmarshaller:unmarshall(<<2, 16, 64, 0, 0, 0, 0, 0, 0, 0, 23, 21, 35, 69, 103, 137, 1, 35, 69, 96>>),
+	"0210" = iso8583_message:get(0, Msg1),
+	[0, 2] = iso8583_message:get_fields(Msg1).
+
+										   
 
 
