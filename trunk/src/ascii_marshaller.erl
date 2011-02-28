2c2cc4541bdf4f04bcd18ab993ad351ab5348ccc
hammingweight <hammingweight@cc4b8374-00f8-dcee-eb2c-e192c0053825>
2011-02-28 17:02:20 +0000

    Can use a custom field marshaller for ascii marshaller.

--- trunk/src/ascii_marshaller.erl
+++ trunk/src/ascii_marshaller.erl
@@ -52,35 +52,38 @@ encode([], _Msg, Result, _EncodingRules) ->
 encode([Field|Tail], Msg, Result, EncodingRules) ->
 	Encoding = EncodingRules:get_encoding(Field),
 	Value = iso8583_message:get(Field, Msg),
-	EncodedValue = encode_field(Encoding, Value),
+	EncodedValue = encode_field(Field, Encoding, Value),
 	encode(Tail, Msg, lists:reverse(EncodedValue) ++ Result, EncodingRules).
 
-encode_field({n, llvar, Length}, Value) when length(Value) =< Length ->
+encode_field(_Field, {n, llvar, Length}, Value) when length(Value) =< Length ->
 	convert:integer_to_string(length(Value), 2) ++ Value;
-encode_field({n, lllvar, Length}, Value) when length(Value) =< Length ->
+encode_field(_Field, {n, lllvar, Length}, Value) when length(Value) =< Length ->
 	convert:integer_to_string(length(Value), 3) ++ Value;
-encode_field({ns, llvar, Length}, Value) when length(Value) =< Length ->
+encode_field(_Field, {ns, llvar, Length}, Value) when length(Value) =< Length ->
 	convert:integer_to_string(length(Value), 2) ++ Value;
-encode_field({an, llvar, Length}, Value) when length(Value) =< Length ->
+encode_field(_Field, {an, llvar, Length}, Value) when length(Value) =< Length ->
 	convert:integer_to_string(length(Value), 2) ++ Value;
-encode_field({an, lllvar, Length}, Value) when length(Value) =< Length ->
+encode_field(_Field, {an, lllvar, Length}, Value) when length(Value) =< Length ->
 	convert:integer_to_string(length(Value), 3) ++ Value;
-encode_field({ans, llvar, Length}, Value) when length(Value) =< Length ->
+encode_field(_Field, {ans, llvar, Length}, Value) when length(Value) =< Length ->
 	convert:integer_to_string(length(Value), 2) ++ Value;
-encode_field({ans, lllvar, Length}, Value) when length(Value) =< Length ->
+encode_field(_Field, {ans, lllvar, Length}, Value) when length(Value) =< Length ->
 	convert:integer_to_string(length(Value), 3) ++ Value;
-encode_field({n, fixed, Length}, Value) when length(Value) =< Length ->
+encode_field(_Field, {n, fixed, Length}, Value) when length(Value) =< Length ->
 	IntValue = list_to_integer(Value),
 	convert:integer_to_string(IntValue, Length);
-encode_field({an, fixed, Length}, Value) when length(Value) =< Length ->
+encode_field(_Field, {an, fixed, Length}, Value) when length(Value) =< Length ->
 	convert:pad_with_trailing_spaces(Value, Length);
-encode_field({ans, fixed, Length}, Value) when length(Value) =< Length ->
+encode_field(_Field, {ans, fixed, Length}, Value) when length(Value) =< Length ->
 	convert:pad_with_trailing_spaces(Value, Length);
-encode_field({x_n, fixed, Length}, [Head | Value]) when Head =:= $C orelse Head =:= $D ->
+encode_field(_Field, {x_n, fixed, Length}, [Head | Value]) when Head =:= $C orelse Head =:= $D ->
 	IntValue = list_to_integer(Value),
 	[Head] ++ convert:integer_to_string(IntValue, Length);
-encode_field({z, llvar, Length}, Value) when length(Value) =< Length ->
+encode_field(_Field, {z, llvar, Length}, Value) when length(Value) =< Length ->
 	convert:integer_to_string(length(Value), 2) ++ Value;
-encode_field({b, Length}, Value) when size(Value) =:= Length ->
-	convert:binary_to_ascii_hex(Value).
+encode_field(_Field, {b, Length}, Value) when size(Value) =:= Length ->
+	convert:binary_to_ascii_hex(Value);
+encode_field(Field, {custom, Marshaller}, Value) ->
+	Marshaller:marshal(Field, Value).
+
 	
