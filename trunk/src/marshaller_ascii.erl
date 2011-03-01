e9a40d8567f3c079b364b1ac6dc2dbb80fda5e5c
hammingweight <hammingweight@cc4b8374-00f8-dcee-eb2c-e192c0053825>
2011-03-01 05:09:47 +0000

    Refactoring ascii_marshaller and ascii_unmarshaller into marshaller_ascii.

--- trunk/src/marshaller_ascii.erl
+++ trunk/src/marshaller_ascii.erl
@@ -1,20 +1,29 @@
 %% Author: carl
-%% Created: 13 Feb 2011
-%% Description: TODO: Add description to ascii_unmarshaller
--module(ascii_unmarshaller).
+%% Created: 12 Feb 2011
+%% Description: TODO: Add description to ascii_marshaller
+-module(marshaller_ascii).
 
 %%
 %% Include files
 %%
 
+
 %%
 %% Exported Functions
 %%
--export([unmarshal/1, unmarshal/2]).
+-export([marshal/1, marshal/2, unmarshal/1, unmarshal/2]).
 
 %%
 %% API Functions
 %%
+marshal(Msg) ->
+	marshal(Msg, iso8583_fields).
+
+marshal(Msg, EncodingRules) ->
+	Mti = iso8583_message:get(0, Msg),
+	[0|Fields] = iso8583_message:get_fields(Msg),
+	Mti ++ bitmap(Fields) ++ encode(Fields, Msg, EncodingRules).
+	
 unmarshal(Msg) ->
 	unmarshal(Msg, iso8583_fields).
 
@@ -25,10 +34,68 @@ unmarshal(Msg, EncodingRules) ->
 	{FieldIds, Fields} = extract_fields(Rest),
 	decode_fields(FieldIds, Fields, IsoMsg2, EncodingRules).
 
-
 %%
 %% Local Functions
 %%
+bitmap([]) ->
+	[];
+bitmap(Fields) ->
+	NumBitMaps = (lists:max(Fields) + 63) div 64,
+	ExtensionBits = [Bit * 64 - 127 || Bit <- lists:seq(2, NumBitMaps)],
+	BitMap = lists:duplicate(NumBitMaps * 8, 0),
+	convert:string_to_ascii_hex(bitmap(lists:sort(ExtensionBits ++ Fields), BitMap)).
+
+bitmap([], Result) ->
+	Result;
+bitmap([Field|Tail], Result) when Field > 0 ->
+	ByteNum = (Field - 1) div 8,
+	BitNum = 7 - ((Field - 1) rem 8),
+	{Left, Right} = lists:split(ByteNum, Result),
+	[ToUpdate | RightRest] = Right,
+	bitmap(Tail, Left ++ ([ToUpdate + (1 bsl BitNum)]) ++ RightRest).
+
+encode(Fields, Msg, EncodingRules) ->
+	lists:reverse(encode(Fields, Msg, [], EncodingRules)).
+
+encode([], _Msg, Result, _EncodingRules) ->
+	Result;
+encode([Field|Tail], Msg, Result, EncodingRules) ->
+	Encoding = EncodingRules:get_encoding(Field),
+	Value = iso8583_message:get(Field, Msg),
+	EncodedValue = encode_field(Field, Encoding, Value),
+	encode(Tail, Msg, lists:reverse(EncodedValue) ++ Result, EncodingRules).
+
+encode_field(_Field, {n, llvar, Length}, Value) when length(Value) =< Length ->
+	convert:integer_to_string(length(Value), 2) ++ Value;
+encode_field(_Field, {n, lllvar, Length}, Value) when length(Value) =< Length ->
+	convert:integer_to_string(length(Value), 3) ++ Value;
+encode_field(_Field, {ns, llvar, Length}, Value) when length(Value) =< Length ->
+	convert:integer_to_string(length(Value), 2) ++ Value;
+encode_field(_Field, {an, llvar, Length}, Value) when length(Value) =< Length ->
+	convert:integer_to_string(length(Value), 2) ++ Value;
+encode_field(_Field, {an, lllvar, Length}, Value) when length(Value) =< Length ->
+	convert:integer_to_string(length(Value), 3) ++ Value;
+encode_field(_Field, {ans, llvar, Length}, Value) when length(Value) =< Length ->
+	convert:integer_to_string(length(Value), 2) ++ Value;
+encode_field(_Field, {ans, lllvar, Length}, Value) when length(Value) =< Length ->
+	convert:integer_to_string(length(Value), 3) ++ Value;
+encode_field(_Field, {n, fixed, Length}, Value) when length(Value) =< Length ->
+	IntValue = list_to_integer(Value),
+	convert:integer_to_string(IntValue, Length);
+encode_field(_Field, {an, fixed, Length}, Value) when length(Value) =< Length ->
+	convert:pad_with_trailing_spaces(Value, Length);
+encode_field(_Field, {ans, fixed, Length}, Value) when length(Value) =< Length ->
+	convert:pad_with_trailing_spaces(Value, Length);
+encode_field(_Field, {x_n, fixed, Length}, [Head | Value]) when Head =:= $C orelse Head =:= $D ->
+	IntValue = list_to_integer(Value),
+	[Head] ++ convert:integer_to_string(IntValue, Length);
+encode_field(_Field, {z, llvar, Length}, Value) when length(Value) =< Length ->
+	convert:integer_to_string(length(Value), 2) ++ Value;
+encode_field(_Field, {b, Length}, Value) when size(Value) =:= Length ->
+	convert:binary_to_ascii_hex(Value);
+encode_field(Field, {custom, Marshaller}, Value) ->
+	Marshaller:marshal(Field, Value).
+
 extract_fields([]) ->
 	{[], []};
 extract_fields(Message) ->
@@ -110,5 +177,3 @@ get_bit_map_length(Msg, Length) ->
 			{_Msg1, Msg2} = lists:split(16, Msg),
 			get_bit_map_length(Msg2, Length+16)
 	end.
-
-	
--- trunk/src/test_ascii_marshaller.erl
+++ trunk/src/test_ascii_marshaller.erl
@@ -40,33 +40,33 @@ unmarshal(_Field, _Value) ->
 mti_only_test() ->
 	Msg1 = iso8583_message:new(),
 	Msg2 = iso8583_message:set(0, "0200", Msg1),
-	"0200" ++ X = ascii_marshaller:marshal(Msg2),
+	"0200" ++ X = marshaller_ascii:marshal(Msg2),
 	Msg3 = iso8583_message:set(0, "0210", Msg1),
-	"0210" ++ X = ascii_marshaller:marshal(Msg3).
+	"0210" ++ X = marshaller_ascii:marshal(Msg3).
 
 pan_test() ->
 	Msg1 = iso8583_message:new(),
 	Msg2 = iso8583_message:set(0, "0200", Msg1),
 	Msg3 = iso8583_message:set(2, "5234567890123456", Msg2),	
-	"02004000000000000000165234567890123456" = ascii_marshaller:marshal(Msg3).
+	"02004000000000000000165234567890123456" = marshaller_ascii:marshal(Msg3).
 
 proc_code_test() ->
 	Msg1 = iso8583_message:new(),
 	Msg2 = iso8583_message:set(0, "0100", Msg1),
 	Msg3 = iso8583_message:set(3, "01234", Msg2),	
-	"01002000000000000000001234" = ascii_marshaller:marshal(Msg3).
+	"01002000000000000000001234" = marshaller_ascii:marshal(Msg3).
 
 amount_tran_test() ->
 	Msg1 = iso8583_message:new(),
 	Msg2 = iso8583_message:set(0, "0200", Msg1),
 	Msg3 = iso8583_message:set(4, "30000", Msg2),	
-	"02001000000000000000000000030000" = ascii_marshaller:marshal(Msg3).
+	"02001000000000000000000000030000" = marshaller_ascii:marshal(Msg3).
 	
 amount_settle_test() ->
 	Msg1 = iso8583_message:new(),
 	Msg2 = iso8583_message:set(0, "0200", Msg1),
 	Msg3 = iso8583_message:set(5, "1", Msg2),	
-	"02000800000000000000000000000001" = ascii_marshaller:marshal(Msg3).
+	"02000800000000000000000000000001" = marshaller_ascii:marshal(Msg3).
 
 fields_5_6_7_test() ->
 	Msg1 = iso8583_message:new(),
@@ -74,7 +74,7 @@ fields_5_6_7_test() ->
 	Msg3 = iso8583_message:set(5, "1", Msg2),	
 	Msg4 = iso8583_message:set(6, "2", Msg3),	
 	Msg5 = iso8583_message:set(7, "0131081200", Msg4),	
-	"02000E000000000000000000000000010000000000020131081200" = ascii_marshaller:marshal(Msg5).
+	"02000E000000000000000000000000010000000000020131081200" = marshaller_ascii:marshal(Msg5).
 
 fields_8_9_10_test() ->
 	Msg1 = iso8583_message:new(),
@@ -82,7 +82,7 @@ fields_8_9_10_test() ->
 	Msg3 = iso8583_message:set(8, "1", Msg2),	
 	Msg4 = iso8583_message:set(9, "2", Msg3),	
 	Msg5 = iso8583_message:set(10, "3", Msg4),	
-	"130001C0000000000000000000010000000200000003" = ascii_marshaller:marshal(Msg5).
+	"130001C0000000000000000000010000000200000003" = marshaller_ascii:marshal(Msg5).
 
 fields_11_12_13_14_test() ->
 	Msg1 = iso8583_message:new(),
@@ -91,7 +91,7 @@ fields_11_12_13_14_test() ->
 	Msg4 = iso8583_message:set(12, "150755", Msg3),	
 	Msg5 = iso8583_message:set(13, "2012", Msg4),
 	Msg6 = iso8583_message:set(14, "1206", Msg5),
-	"0200003C00000000000000123415075520121206" = ascii_marshaller:marshal(Msg6).
+	"0200003C00000000000000123415075520121206" = marshaller_ascii:marshal(Msg6).
 
 fields_21_to_25_test() ->
 	Msg1 = iso8583_message:new(),
@@ -101,7 +101,7 @@ fields_21_to_25_test() ->
 	Msg5 = iso8583_message:set(?APPLICATION_PAN_NUMBER, "3", Msg4),
 	Msg6 = iso8583_message:set(?POS_CONDITION_CODE, "5", Msg5),
 	Msg7 = iso8583_message:set(?FUNCTION_CODE, "4", Msg6),
-	"020000000F800000000000100200300405" = ascii_marshaller:marshal(Msg7).
+	"020000000F800000000000100200300405" = marshaller_ascii:marshal(Msg7).
 	
 fields_27_to_33_test() ->
 	Msg1 = iso8583_message:new(),
@@ -114,7 +114,7 @@ fields_27_to_33_test() ->
 	Msg8 = iso8583_message:set(32, "555", Msg7),
 	Msg9 = iso8583_message:set(33, "12345678901", Msg8),
 	"02000000003F800000009C00000011D00000022C00000033C00000044035551112345678901"
-		= ascii_marshaller:marshal(Msg9).
+		= marshaller_ascii:marshal(Msg9).
 
 fields_34_35_test() ->
 	Msg1 = iso8583_message:new(),
@@ -122,7 +122,7 @@ fields_34_35_test() ->
 	Msg3 = iso8583_message:set(34, "12341234123412341234567890", Msg2),	
 	Msg4 = iso8583_message:set(35, ";1234123412341234=0305101193010877?", Msg3),
 	"02000000000060000000261234123412341234123456789035;1234123412341234=0305101193010877?" =
-		ascii_marshaller:marshal(Msg4).
+		marshaller_ascii:marshal(Msg4).
 
 fields_36_37_38_test() ->
 	Msg1 = iso8583_message:new(),
@@ -130,63 +130,63 @@ fields_36_37_38_test() ->
 	Msg3 = iso8583_message:set(36, "1234567890", Msg2),	
 	Msg4 = iso8583_message:set(37, "Query123456", Msg3),
 	Msg5 = iso8583_message:set(38, "123", Msg4),
-	"0200000000001C0000000101234567890Query123456 123   " = ascii_marshaller:marshal(Msg5).
+	"0200000000001C0000000101234567890Query123456 123   " = marshaller_ascii:marshal(Msg5).
 
 field_39_test() ->
 	Msg1 = iso8583_message:new(),
 	Msg2 = iso8583_message:set(0, "0200", Msg1),
 	Msg3 = iso8583_message:set(39, "R7", Msg2),
-	"02000000000002000000R7" = ascii_marshaller:marshal(Msg3).
+	"02000000000002000000R7" = marshaller_ascii:marshal(Msg3).
 
 field_40_test() ->
 	Msg1 = iso8583_message:new(),
 	Msg2 = iso8583_message:set(0, "0200", Msg1),
 	Msg3 = iso8583_message:set(?SERVICE_RESTRICTION_CODE, "R 1", Msg2),
-	"02000000000001000000R 1" = ascii_marshaller:marshal(Msg3).
+	"02000000000001000000R 1" = marshaller_ascii:marshal(Msg3).
 	
 field_41_test() ->
 	Msg1 = iso8583_message:new(),
 	Msg2 = iso8583_message:set(0, "0200", Msg1),
 	Msg3 = iso8583_message:set(41, "Term#1", Msg2),
-	"02000000000000800000Term#1  " = ascii_marshaller:marshal(Msg3).
+	"02000000000000800000Term#1  " = marshaller_ascii:marshal(Msg3).
 
 field_42_test() ->
 	Msg1 = iso8583_message:new(),
 	Msg2 = iso8583_message:set(0, "0200", Msg1),
 	Msg3 = iso8583_message:set(?CARD_ACCEPTOR_ID_CODE, "CA ID 123", Msg2),
-	"02000000000000400000CA ID 123      " = ascii_marshaller:marshal(Msg3).
+	"02000000000000400000CA ID 123      " = marshaller_ascii:marshal(Msg3).
 
 field_43_test() ->
 	Msg1 = iso8583_message:new(),
 	Msg2 = iso8583_message:set(0, "0200", Msg1),
 	Msg3 = iso8583_message:set(43, "NAME                                  ZA", Msg2),
-	"02000000000000200000NAME                                  ZA" = ascii_marshaller:marshal(Msg3).
+	"02000000000000200000NAME                                  ZA" = marshaller_ascii:marshal(Msg3).
 
 field_44_test() ->
 	Msg1 = iso8583_message:new(),
 	Msg2 = iso8583_message:set(0, "0200", Msg1),
 	Msg3 = iso8583_message:set(44, "Foo123", Msg2),
-	"0200000000000010000006Foo123" = ascii_marshaller:marshal(Msg3).
+	"0200000000000010000006Foo123" = marshaller_ascii:marshal(Msg3).
 	
 field_45_test() ->
 	Msg1 = iso8583_message:new(),
 	Msg2 = iso8583_message:set(0, "0200", Msg1),
 	Msg3 = iso8583_message:set(?TRACK_1_DATA, "Foo123", Msg2),
-	"0200000000000008000006Foo123" = ascii_marshaller:marshal(Msg3).
+	"0200000000000008000006Foo123" = marshaller_ascii:marshal(Msg3).
 	
 field_46_test() ->
 	Msg1 = iso8583_message:new(),
 	Msg2 = iso8583_message:set(0, "0200", Msg1),
 	Msg3 = iso8583_message:set(2, "5234567890123456789", Msg2),
 	Msg4 = iso8583_message:set(?ADDITIONAL_DATA_ISO, "Hello, world!", Msg3),
-	"02004000000000040000195234567890123456789013Hello, world!" = ascii_marshaller:marshal(Msg4).
+	"02004000000000040000195234567890123456789013Hello, world!" = marshaller_ascii:marshal(Msg4).
 
 fields_47_48_test() ->
 	Msg1 = iso8583_message:new(),
 	Msg2 = iso8583_message:set(0, "0200", Msg1),
 	Msg3 = iso8583_message:set(47, "Hello!", Msg2),
 	Msg4 = iso8583_message:set(48, "Goodbye!", Msg3),
-	"02000000000000030000006Hello!008Goodbye!" = ascii_marshaller:marshal(Msg4).
+	"02000000000000030000006Hello!008Goodbye!" = marshaller_ascii:marshal(Msg4).
 
 fields_49_50_51_test() ->
 	Msg1 = iso8583_message:new(),
@@ -194,27 +194,27 @@ fields_49_50_51_test() ->
 	Msg3 = iso8583_message:set(?CURRENCY_CODE_TRAN, "A", Msg2),
 	Msg4 = iso8583_message:set(?CURRENCY_CODE_SETTLE, "B", Msg3),
 	Msg5 = iso8583_message:set(?CURRENCY_CODE_CARDHOLDER_BILLING, "C", Msg4),
-	"0200000000000000E000A  B  C  " = ascii_marshaller:marshal(Msg5).
+	"0200000000000000E000A  B  C  " = marshaller_ascii:marshal(Msg5).
 
 field_52_test() ->
 	Msg1 = iso8583_message:new(),
 	Msg2 = iso8583_message:set(0, "0200", Msg1),
 	Msg3 = iso8583_message:set(52, <<253, 0, 1, 2, 3, 4, 5, 127>>, Msg2),
-	"02000000000000001000FD0001020304057F" = ascii_marshaller:marshal(Msg3).
+	"02000000000000001000FD0001020304057F" = marshaller_ascii:marshal(Msg3).
 
 field_53_test() ->
 	Msg1 = iso8583_message:new(),
 	Msg2 = iso8583_message:set(0, "0200", Msg1),
 	Msg3 = iso8583_message:set(2, "5234567890123456789", Msg2),
 	Msg4 = iso8583_message:set(?SECURITY_RELATED_CONTROL_INFO, "17", Msg3),
-	"020040000000000008001952345678901234567890000000000000017" = ascii_marshaller:marshal(Msg4).
+	"020040000000000008001952345678901234567890000000000000017" = marshaller_ascii:marshal(Msg4).
 	
 field_54_test() ->
 	Msg1 = iso8583_message:new(),
 	Msg2 = iso8583_message:set(0, "0200", Msg1),
 	Msg3 = iso8583_message:set(2, "5234567890123456789", Msg2),
 	Msg4 = iso8583_message:set(?ADDITIONAL_AMOUNTS, "Additi0nal Am0unt", Msg3),
-	"02004000000000000400195234567890123456789017Additi0nal Am0unt" = ascii_marshaller:marshal(Msg4).
+	"02004000000000000400195234567890123456789017Additi0nal Am0unt" = marshaller_ascii:marshal(Msg4).
 
 fields_55_56_57_test() ->
 	Msg1 = iso8583_message:new(),
@@ -223,20 +223,20 @@ fields_55_56_57_test() ->
 	Msg4 = iso8583_message:set(55, "A1", Msg3),
 	Msg5 = iso8583_message:set(56, "B22", Msg4),
 	Msg6 = iso8583_message:set(57, "C333", Msg5),
-	"02004000000000000380195234567890123456789002A1003B22004C333" = ascii_marshaller:marshal(Msg6).
+	"02004000000000000380195234567890123456789002A1003B22004C333" = marshaller_ascii:marshal(Msg6).
 	
 field_64_test() ->
 	Msg1 = iso8583_message:new(),
 	Msg2 = iso8583_message:set(0, "0200", Msg1),
 	Msg3 = iso8583_message:set(64, <<128, 255, 1, 2, 3, 4, 5, 127>>, Msg2),
-	"0200000000000000000180FF01020304057F" = ascii_marshaller:marshal(Msg3).
+	"0200000000000000000180FF01020304057F" = marshaller_ascii:marshal(Msg3).
 
 field_66_test() ->
 	Msg1 = iso8583_message:new(),
 	Msg2 = iso8583_message:set(0, "0200", Msg1),
 	Msg3 = iso8583_message:set(?SETTLE_CODE, "9", Msg2),
-	"0200800000000000000040000000000000009" = ascii_marshaller:marshal(Msg3),
-	Msg3 = ascii_unmarshaller:unmarshal(ascii_marshaller:marshal(Msg3)).
+	"0200800000000000000040000000000000009" = marshaller_ascii:marshal(Msg3),
+	Msg3 = ascii_unmarshaller:unmarshal(marshaller_ascii:marshal(Msg3)).
 	
 fields_67_to_70_test() ->
 	Msg1 = iso8583_message:new(),
@@ -245,14 +245,14 @@ fields_67_to_70_test() ->
 	Msg4 = iso8583_message:set(?RECEIVING_INSTITUTION_COUNTRY_CODE, "2", Msg3),
 	Msg5 = iso8583_message:set(?SETTLE_INSTITUTION_COUNTRY_CODE, "3", Msg4),
 	Msg6 = iso8583_message:set(?NETWORK_MANAGEMENT_INFORMATION_CODE, "4", Msg5),
-	"020080000000000000003C0000000000000001002003004" = ascii_marshaller:marshal(Msg6).
+	"020080000000000000003C0000000000000001002003004" = marshaller_ascii:marshal(Msg6).
 	
 fields_71_72_test() ->
 	Msg1 = iso8583_message:new(),
 	Msg2 = iso8583_message:set(0, "0200", Msg1),
 	Msg3 = iso8583_message:set(?MESSAGE_NUMBER, "1", Msg2),
 	Msg4 = iso8583_message:set(?MESSAGE_NUMBER_LAST, "2", Msg3),
-	"02008000000000000000030000000000000000010002" = ascii_marshaller:marshal(Msg4).
+	"02008000000000000000030000000000000000010002" = marshaller_ascii:marshal(Msg4).
 
 fields_74_to_77_test() ->
 	Msg1 = iso8583_message:new(),
@@ -261,7 +261,7 @@ fields_74_to_77_test() ->
 	Msg4 = iso8583_message:set(?CREDITS_REVERSAL_NUMBER, "22", Msg3),
 	Msg5 = iso8583_message:set(?DEBITS_NUMBER, "333", Msg4),
 	Msg6 = iso8583_message:set(?DEBITS_REVERSAL_NUMBER, "4444", Msg5),
-	"0200800000000000000000780000000000000000000001000000002200000003330000004444" = ascii_marshaller:marshal(Msg6).
+	"0200800000000000000000780000000000000000000001000000002200000003330000004444" = marshaller_ascii:marshal(Msg6).
 
 fields_83_to_90_test() ->
 	Msg1 = iso8583_message:new(),
@@ -274,40 +274,40 @@ fields_83_to_90_test() ->
 	Msg8 = iso8583_message:set(?DEBITS_AMOUNT, "666666", Msg7),
 	Msg9 = iso8583_message:set(?DEBITS_REVERSAL_AMOUNT, "7777777", Msg8),
 	Msg10 = iso8583_message:set(?ORIGINAL_DATA_ELEMENTS, "88888888", Msg9),
-	"0200800000000000000000003FC0000000000000000000010000000000220000000003330000000000004444000000000005555500000000006666660000000007777777000000000000000000000000000000000088888888" = ascii_marshaller:marshal(Msg10).
+	"0200800000000000000000003FC0000000000000000000010000000000220000000003330000000000004444000000000005555500000000006666660000000007777777000000000000000000000000000000000088888888" = marshaller_ascii:marshal(Msg10).
 
 fields_95_96_test() ->
 	Msg1 = iso8583_message:new(),
 	Msg2 = iso8583_message:set(0, "0200", Msg1),
 	Msg3 = iso8583_message:set(?REPLACEMENT_AMOUNTS, "1", Msg2),
 	Msg4 = iso8583_message:set(?MESSAGE_SECURITY_CODE, <<1,0,0,0,0,0,0,255>>, Msg3),
-	"0200800000000000000000000003000000001                                         01000000000000FF" = ascii_marshaller:marshal(Msg4).
+	"0200800000000000000000000003000000001                                         01000000000000FF" = marshaller_ascii:marshal(Msg4).
 	
 fields_99_100_test() ->
 	Msg1 = iso8583_message:new(),
 	Msg2 = iso8583_message:set(0, "0200", Msg1),
 	Msg3 = iso8583_message:set(?SETTLE_INSTITUTION_ID_CODE, "11", Msg2),
 	Msg4 = iso8583_message:set(?RECEIVING_INSTITUTION_ID_CODE, "2222", Msg3),
-	"0200800000000000000000000000300000000211042222" = ascii_marshaller:marshal(Msg4).
+	"0200800000000000000000000000300000000211042222" = marshaller_ascii:marshal(Msg4).
 	
 field_101_test() ->
 	Msg1 = iso8583_message:new(),
 	Msg2 = iso8583_message:set(0, "0200", Msg1),
 	Msg3 = iso8583_message:set(?FILE_NAME, "A loong file name", Msg2),
-	"02008000000000000000000000000800000017A loong file name" = ascii_marshaller:marshal(Msg3).
+	"02008000000000000000000000000800000017A loong file name" = marshaller_ascii:marshal(Msg3).
 	
 encoding_rules_test() ->
 	Msg1 = iso8583_message:new(),
 	Msg2 = iso8583_message:set(0, "0200", Msg1),
 	Msg3 = iso8583_message:set(2, "1", Msg2),
-	"020040000000000000000001" = ascii_marshaller:marshal(Msg3, ?MODULE).
+	"020040000000000000000001" = marshaller_ascii:marshal(Msg3, ?MODULE).
 	
 custom_marshaller_test() ->
 	Msg1 = iso8583_message:new(),
 	Msg2 = iso8583_message:set(0, "0200", Msg1),
 	Msg3 = iso8583_message:set(3, "1", Msg2),
 	Msg4 = iso8583_message:set(4, "4", Msg3),
-	"02003000000000000000Field 3Field 4" = ascii_marshaller:marshal(Msg4, ?MODULE).
+	"02003000000000000000Field 3Field 4" = marshaller_ascii:marshal(Msg4, ?MODULE).
 	
 %%
 %% Local Functions
