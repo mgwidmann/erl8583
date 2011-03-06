a884694f36da86565a2e9a517e4d5d078a60b7a5
hammingweight <hammingweight@cc4b8374-00f8-dcee-eb2c-e192c0053825>
2011-03-06 07:50:32 +0000

    Refactoring.

--- trunk/src/marshaller_binary.erl
+++ trunk/src/marshaller_binary.erl
@@ -10,7 +10,14 @@
 %%
 %% Exported Functions
 %%
--export([marshal/1, marshal/2, unmarshal/1, unmarshal/2, construct_bitmap/1, extract_fields/1]).
+-export([marshal/1, 
+		 marshal/2, 
+		 unmarshal/1, 
+		 unmarshal/2, 
+		 construct_bitmap/1, 
+		 extract_fields/1,
+		 encode_data_element/2,
+		 decode_data_element/2]).
 
 %%
 %% API Functions
@@ -53,6 +60,106 @@ extract_fields(Message) ->
 	BitMap = binary_to_list(BinaryBitMap),
 	extract_fields(BitMap, 0, 8, {[], Fields}).
 
+encode_data_element({n, llvar, Length}, Value) when length(Value) =< Length ->
+	LField = convert:integer_to_bcd(length(Value), 2),
+	VField = convert:ascii_hex_to_bcd(Value, "0"),
+	convert:concat_binaries(LField, VField);
+encode_data_element({z, llvar, Length}, Value) when length(Value) =< Length ->
+	LField = convert:integer_to_bcd(length(Value), 2),
+	VField = convert:string_to_track2(Value),
+	convert:concat_binaries(LField, VField);
+encode_data_element({n, fixed, Length}, Value) ->
+	case Length rem 2 of
+		0 ->
+			PaddedValue = convert:integer_to_string(list_to_integer(Value), Length);
+		1 ->
+			PaddedValue = convert:integer_to_string(list_to_integer(Value), Length+1)
+	end,
+	convert:ascii_hex_to_bcd(PaddedValue, "0");
+encode_data_element({an, fixed, Length}, Value) ->
+	list_to_binary(convert:pad_with_trailing_spaces(Value, Length));
+encode_data_element({ans, fixed, Length}, Value) ->
+	list_to_binary(convert:pad_with_trailing_spaces(Value, Length));
+encode_data_element({an, llvar, Length}, Value) when length(Value) =< Length ->
+	LField = convert:integer_to_bcd(length(Value), 2),
+	convert:concat_binaries(LField, list_to_binary(Value));
+encode_data_element({ns, llvar, Length}, Value) when length(Value) =< Length ->
+	LField = convert:integer_to_bcd(length(Value), 2),
+	convert:concat_binaries(LField, list_to_binary(Value));
+encode_data_element({ans, llvar, Length}, Value) when length(Value) =< Length ->
+	LField = convert:integer_to_bcd(length(Value), 2),
+	convert:concat_binaries(LField, list_to_binary(Value));
+encode_data_element({ans, lllvar, Length}, Value) when length(Value) =< Length ->
+	LField = convert:integer_to_bcd(length(Value), 3),
+	convert:concat_binaries(LField, list_to_binary(Value));
+encode_data_element({x_n, fixed, Length}, [Head | Value]) when Head =:= $C orelse Head =:= $D ->
+	IntValue = list_to_integer(Value),
+	convert:concat_binaries(<<Head>>,  convert:integer_to_bcd(IntValue, Length));
+encode_data_element({b, Length}, Value) when size(Value) =:= Length ->
+	Value.
+
+decode_data_element({n, llvar, _MaxLength}, Fields) ->
+	{NBin, RestBin} = split_binary(Fields, 1),
+	N = convert:bcd_to_integer(NBin),
+	{ValueBin, Rest} = split_binary(RestBin, (N+1) div 2), 
+	{convert:bcd_to_ascii_hex(ValueBin, N, "0"), Rest};
+decode_data_element({n, lllvar, _MaxLength}, Fields) ->
+	{NBin, RestBin} = split_binary(Fields, 2),
+	N = convert:bcd_to_integer(NBin),
+	{ValueBin, Rest} = split_binary(RestBin, (N+1) div 2), 
+	{convert:bcd_to_ascii_hex(ValueBin, N, "0"), Rest};
+decode_data_element({an, llvar, _MaxLength}, Fields) ->
+	{NBin, RestBin} = split_binary(Fields, 1),
+	N = convert:bcd_to_integer(NBin),
+	{ValueBin, Rest} = split_binary(RestBin, N), 
+	{binary_to_list(ValueBin), Rest};
+decode_data_element({ns, llvar, _MaxLength}, Fields) ->
+	{NBin, RestBin} = split_binary(Fields, 1),
+	N = convert:bcd_to_integer(NBin),
+	{ValueBin, Rest} = split_binary(RestBin, N), 
+	{binary_to_list(ValueBin), Rest};
+decode_data_element({ans, llvar, _MaxLength}, Fields) ->
+	{NBin, RestBin} = split_binary(Fields, 1),
+	N = convert:bcd_to_integer(NBin),
+	{ValueBin, Rest} = split_binary(RestBin, N), 
+	{binary_to_list(ValueBin), Rest};
+decode_data_element({ans, lllvar, _MaxLength}, Fields) ->
+	{NBin, RestBin} = split_binary(Fields, 2),
+	N = convert:bcd_to_integer(NBin),
+	{ValueBin, Rest} = split_binary(RestBin, N), 
+	{binary_to_list(ValueBin), Rest};
+decode_data_element({n, fixed, Length}, Fields) ->
+	{NBin, RestBin} = split_binary(Fields, (Length + 1) div 2),
+	case Length rem 2 of
+		0 ->
+			{convert:bcd_to_ascii_hex(NBin, Length, "0"), RestBin};
+		1 ->
+			[$0|AsciiHex] = convert:bcd_to_ascii_hex(NBin, Length+1, "0"),
+			{AsciiHex, RestBin}
+	end;
+decode_data_element({an, fixed, Length}, Fields) ->
+	{FieldBin, RestBin} = split_binary(Fields, Length),
+	FieldStr = binary_to_list(FieldBin),
+	{convert:pad_with_trailing_spaces(FieldStr, Length), RestBin};
+decode_data_element({ans, fixed, Length}, Fields) ->
+	{FieldBin, RestBin} = split_binary(Fields, Length),
+	FieldStr = binary_to_list(FieldBin),
+	{convert:pad_with_trailing_spaces(FieldStr, Length), RestBin};
+decode_data_element({x_n, fixed, Length}, Fields) ->
+	{FieldBin, RestBin} = split_binary(Fields, Length div 2 + 1),
+	{<<X>>, Value} = split_binary(FieldBin, 1),
+	ValueStr = convert:bcd_to_ascii_hex(Value, Length, "0"),
+	case X =:= $C orelse X =:= $D of
+		true ->
+			{[X] ++ ValueStr, RestBin}
+	end;
+decode_data_element({z, llvar, _MaxLength}, Fields) ->
+	{NBin, RestBin} = split_binary(Fields, 1),
+	N = convert:bcd_to_integer(NBin),
+	{ValueBin, Rest} = split_binary(RestBin, (N+1) div 2), 
+	{convert:track2_to_string(ValueBin, N), Rest};
+decode_data_element({b, Length}, Fields) ->
+	split_binary(Fields, Length).
 
 %%
 %% Local Functions
@@ -77,45 +184,10 @@ encode([Field|Tail], Msg, Result, EncodingRules) ->
 	EncodedValue = encode_field(Field, Encoding, Value),
 	encode(Tail, Msg, convert:concat_binaries(Result, EncodedValue), EncodingRules).
 
-encode_field(_FieldId, {n, llvar, Length}, Value) when length(Value) =< Length ->
-	LField = convert:integer_to_bcd(length(Value), 2),
-	VField = convert:ascii_hex_to_bcd(Value, "0"),
-	convert:concat_binaries(LField, VField);
-encode_field(_FieldId, {z, llvar, Length}, Value) when length(Value) =< Length ->
-	LField = convert:integer_to_bcd(length(Value), 2),
-	VField = convert:string_to_track2(Value),
-	convert:concat_binaries(LField, VField);
-encode_field(_FieldId, {n, fixed, Length}, Value) ->
-	case Length rem 2 of
-		0 ->
-			PaddedValue = convert:integer_to_string(list_to_integer(Value), Length);
-		1 ->
-			PaddedValue = convert:integer_to_string(list_to_integer(Value), Length+1)
-	end,
-	convert:ascii_hex_to_bcd(PaddedValue, "0");
-encode_field(_FieldId, {an, fixed, Length}, Value) ->
-	list_to_binary(convert:pad_with_trailing_spaces(Value, Length));
-encode_field(_FieldId, {ans, fixed, Length}, Value) ->
-	list_to_binary(convert:pad_with_trailing_spaces(Value, Length));
-encode_field(_FieldId, {an, llvar, Length}, Value) when length(Value) =< Length ->
-	LField = convert:integer_to_bcd(length(Value), 2),
-	convert:concat_binaries(LField, list_to_binary(Value));
-encode_field(_FieldId, {ns, llvar, Length}, Value) when length(Value) =< Length ->
-	LField = convert:integer_to_bcd(length(Value), 2),
-	convert:concat_binaries(LField, list_to_binary(Value));
-encode_field(_FieldId, {ans, llvar, Length}, Value) when length(Value) =< Length ->
-	LField = convert:integer_to_bcd(length(Value), 2),
-	convert:concat_binaries(LField, list_to_binary(Value));
-encode_field(_FieldId, {ans, lllvar, Length}, Value) when length(Value) =< Length ->
-	LField = convert:integer_to_bcd(length(Value), 3),
-	convert:concat_binaries(LField, list_to_binary(Value));
-encode_field(_FieldId, {x_n, fixed, Length}, [Head | Value]) when Head =:= $C orelse Head =:= $D ->
-	IntValue = list_to_integer(Value),
-	convert:concat_binaries(<<Head>>,  convert:integer_to_bcd(IntValue, Length));
-encode_field(_FieldId, {b, Length}, Value) when size(Value) =:= Length ->
-	Value;
 encode_field(FieldId, {custom, Marshaller}, Value) ->
-	Marshaller:marshal(FieldId, Value).
+	Marshaller:marshal(FieldId, Value);
+encode_field(_FieldId, Pattern, Value) ->
+	marshaller_binary:encode_data_element(Pattern, Value).
 
 get_bit_map_length(Message) ->
 	[Head|_Tail] = binary_to_list(Message),
@@ -148,67 +220,7 @@ decode_fields([Field|Tail], Fields, Result, EncodingRules) ->
 	UpdatedResult = iso8583_message:set(Field, Value, Result),
 	decode_fields(Tail, UpdatedFields, UpdatedResult, EncodingRules).
 	
-decode_field(_FieldId, {n, llvar, _MaxLength}, Fields) ->
-	{NBin, RestBin} = split_binary(Fields, 1),
-	N = convert:bcd_to_integer(NBin),
-	{ValueBin, Rest} = split_binary(RestBin, (N+1) div 2), 
-	{convert:bcd_to_ascii_hex(ValueBin, N, "0"), Rest};
-decode_field(_FieldId, {n, lllvar, _MaxLength}, Fields) ->
-	{NBin, RestBin} = split_binary(Fields, 2),
-	N = convert:bcd_to_integer(NBin),
-	{ValueBin, Rest} = split_binary(RestBin, (N+1) div 2), 
-	{convert:bcd_to_ascii_hex(ValueBin, N, "0"), Rest};
-decode_field(_FieldId, {an, llvar, _MaxLength}, Fields) ->
-	{NBin, RestBin} = split_binary(Fields, 1),
-	N = convert:bcd_to_integer(NBin),
-	{ValueBin, Rest} = split_binary(RestBin, N), 
-	{binary_to_list(ValueBin), Rest};
-decode_field(_FieldId, {ns, llvar, _MaxLength}, Fields) ->
-	{NBin, RestBin} = split_binary(Fields, 1),
-	N = convert:bcd_to_integer(NBin),
-	{ValueBin, Rest} = split_binary(RestBin, N), 
-	{binary_to_list(ValueBin), Rest};
-decode_field(_FieldId, {ans, llvar, _MaxLength}, Fields) ->
-	{NBin, RestBin} = split_binary(Fields, 1),
-	N = convert:bcd_to_integer(NBin),
-	{ValueBin, Rest} = split_binary(RestBin, N), 
-	{binary_to_list(ValueBin), Rest};
-decode_field(_FieldId, {ans, lllvar, _MaxLength}, Fields) ->
-	{NBin, RestBin} = split_binary(Fields, 2),
-	N = convert:bcd_to_integer(NBin),
-	{ValueBin, Rest} = split_binary(RestBin, N), 
-	{binary_to_list(ValueBin), Rest};
-decode_field(_FieldId, {n, fixed, Length}, Fields) ->
-	{NBin, RestBin} = split_binary(Fields, (Length + 1) div 2),
-	case Length rem 2 of
-		0 ->
-			{convert:bcd_to_ascii_hex(NBin, Length, "0"), RestBin};
-		1 ->
-			[$0|AsciiHex] = convert:bcd_to_ascii_hex(NBin, Length+1, "0"),
-			{AsciiHex, RestBin}
-	end;
-decode_field(_FieldId, {an, fixed, Length}, Fields) ->
-	{FieldBin, RestBin} = split_binary(Fields, Length),
-	FieldStr = binary_to_list(FieldBin),
-	{convert:pad_with_trailing_spaces(FieldStr, Length), RestBin};
-decode_field(_FieldId, {ans, fixed, Length}, Fields) ->
-	{FieldBin, RestBin} = split_binary(Fields, Length),
-	FieldStr = binary_to_list(FieldBin),
-	{convert:pad_with_trailing_spaces(FieldStr, Length), RestBin};
-decode_field(_FieldId, {x_n, fixed, Length}, Fields) ->
-	{FieldBin, RestBin} = split_binary(Fields, Length div 2 + 1),
-	{<<X>>, Value} = split_binary(FieldBin, 1),
-	ValueStr = convert:bcd_to_ascii_hex(Value, Length, "0"),
-	case X =:= $C orelse X =:= $D of
-		true ->
-			{[X] ++ ValueStr, RestBin}
-	end;
-decode_field(_FieldId, {z, llvar, _MaxLength}, Fields) ->
-	{NBin, RestBin} = split_binary(Fields, 1),
-	N = convert:bcd_to_integer(NBin),
-	{ValueBin, Rest} = split_binary(RestBin, (N+1) div 2), 
-	{convert:track2_to_string(ValueBin, N), Rest};
-decode_field(_FieldId, {b, Length}, Fields) ->
-	split_binary(Fields, Length);
 decode_field(FieldId, {custom, Marshaller}, Value) ->
-	Marshaller:unmarshal(FieldId, Value).
+	Marshaller:unmarshal(FieldId, Value);
+decode_field(_FieldId, Pattern, Value) ->
+	decode_data_element(Pattern, Value).
