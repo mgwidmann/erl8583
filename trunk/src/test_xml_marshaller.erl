22524d68350b6667b8d2db2b7639d2dfd0d2c888
hammingweight <hammingweight@cc4b8374-00f8-dcee-eb2c-e192c0053825>
2011-03-06 13:52:08 +0000

    Factored out XML field unmarshalling from message unmarshalling.

--- trunk/src/test_xml_marshaller.erl
+++ trunk/src/test_xml_marshaller.erl
@@ -18,36 +18,36 @@
 %%
 
 %% Check that marshalling does the opposite of unmarshalling.
-xml_marshall_empty_test() ->
+xml_marshal_empty_test() ->
 	IsoMsg = iso8583_message:new(),
 	Marshalled = marshaller_xml:marshal(IsoMsg),
 	IsoMsg = marshaller_xml:unmarshal(Marshalled).
 
-xml_marshall_simple_test() ->
+xml_marshal_simple_test() ->
 	IsoMsg1 = iso8583_message:new(),
 	IsoMsg2 = iso8583_message:set(0, "0200", IsoMsg1),
 	Marshalled = marshaller_xml:marshal(IsoMsg2),
 	IsoMsg2 = marshaller_xml:unmarshal(Marshalled).
 
-xml_marshall_complex_test() ->
+xml_marshal_complex_test() ->
 	BitMap = iso8583_message:from_list([{1, "foo"}, {2, "bar"}]),
 	IsoMsg = iso8583_message:from_list([{1, "0200"}, {3, "333333"}, {48, BitMap} ]),
 	Marshalled = marshaller_xml:marshal(IsoMsg),
 	IsoMsg = marshaller_xml:unmarshal(Marshalled).
 
-xml_marshall_with_attributes_test() ->
+xml_marshal_with_attributes_test() ->
 	IsoMsg = iso8583_message:new([{"outgoing", "true"}]),
 	Marshalled = marshaller_xml:marshal(IsoMsg),
 	IsoMsg = marshaller_xml:unmarshal(Marshalled).
 
-xml_marshall_complex_attributes_test() ->
+xml_marshal_complex_attributes_test() ->
 	BitMap = iso8583_message:from_list([{1, "foo"}, {2, "bar"}]),
 	BitMap2 = iso8583_message:set_attributes([{"foo","bar"},{"hello","world"}], BitMap),
 	IsoMsg = iso8583_message:from_list([{1, "0200"}, {3, "333333"}, {48, BitMap2} ]),
 	Marshalled = marshaller_xml:marshal(IsoMsg),
 	IsoMsg = marshaller_xml:unmarshal(Marshalled).
 
-xml_marshall_binary_test() ->
+xml_marshal_binary_test() ->
 	IsoMsg1 = iso8583_message:new(),
 	IsoMsg2 = iso8583_message:set(0, "0200", IsoMsg1),
 	IsoMsg3 = iso8583_message:set(52, <<1,2,3,4,5,6,7,255>>, IsoMsg2),
