4bc32d063d7d9c06a784fd8305013f1c2d49656c
hammingweight <hammingweight@cc4b8374-00f8-dcee-eb2c-e192c0053825>
2011-03-21 17:32:41 +0000

    make script packages distro into a zip archive.

--- make.sh
+++ make.sh
@@ -2,4 +2,5 @@ rm ebin/*.beam
 erlc -I include -o ebin src/*.erl
 erl -noshell -pa ebin -s test_all test -s init stop
 ./gen_doc.sh
+zip dist/erl8583_0.1.0.zip src/* include/* doc/* ebin/* make.sh
 
