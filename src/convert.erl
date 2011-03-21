d9c7bac57817a6dfc9008f111a2f93432a68af88
hammingweight <hammingweight@cc4b8374-00f8-dcee-eb2c-e192c0053825>
2011-03-21 08:41:55 +0000

    Added @spec declaration for convert module.

--- src/convert.erl
+++ src/convert.erl
@@ -48,6 +48,8 @@
 
 %% @doc Converts a string() of characters to a list containing
 %%      the ASCII hex equivalents.
+%%
+%% @spec string_to_ascii_hex(string()) -> string()
 -spec(string_to_ascii_hex(string()) -> string()).
 
 string_to_ascii_hex(Str) ->
@@ -55,6 +57,8 @@ string_to_ascii_hex(Str) ->
 
 %% @doc Converts a string() containing ASCII hex characters
 %%      to an equivalent string().
+%%
+%% @spec ascii_hex_to_string(string()) -> string()
 -spec(ascii_hex_to_string(string()) -> string()).
 
 ascii_hex_to_string(HexStr) ->
@@ -62,6 +66,8 @@ ascii_hex_to_string(HexStr) ->
 
 %% @doc Converts an integer to a string of fixed length with
 %%      leading zeroes if necessary.
+%%
+%% @spec integer_to_string(integer(), integer()) -> string()
 -spec(integer_to_string(integer(), integer()) -> string()).
 
 integer_to_string(Value, Length) ->
@@ -69,18 +75,24 @@ integer_to_string(Value, Length) ->
 
 %% @doc Pads a string with a number of spaces so that the
 %%      resultant string has specified length.
+%%
+%% @spec pad_with_trailing_spaces(string(), integer()) -> string()
 -spec(pad_with_trailing_spaces(string(), integer()) -> string()).
 
 pad_with_trailing_spaces(List, Length) ->
 	lists:reverse(pad_with_leading_spaces(lists:reverse(List), Length)).
 
 %% @doc Returns the ASCII hex encoding of a binary value.
+%%
+%% @spec binary_to_ascii_hex(binary()) -> string()
 -spec(binary_to_ascii_hex(binary()) -> string()).
 
 binary_to_ascii_hex(Bin) ->
 	binary_to_ascii_hex(binary_to_list(Bin), []).
 
 %% @doc Returns the binary value corresponding to an ASCII hex string.
+%%
+%% @spec ascii_hex_to_binary(string()) -> binary()
 -spec(ascii_hex_to_binary(string()) -> binary()).
 
 ascii_hex_to_binary(List) ->
@@ -88,12 +100,16 @@ ascii_hex_to_binary(List) ->
 	list_to_binary(Bytes).
 	
 %% @doc Concatenates a list of binaries and returns the result.
+%%
+%% @spec concat_binaries(list(binary())) -> binary()
 -spec(concat_binaries(list(binary())) -> binary()).
 
 concat_binaries(List) ->
 	lists:foldr({convert, concat_binaries}, <<>>, List).
 
 %% @doc Concatenates two binaries.
+%%
+%% @spec concat_binaries(binary(), binary()) -> binary()
 -spec(concat_binaries(binary(), binary()) -> binary()).
 
 concat_binaries(X, Y) ->
@@ -101,6 +117,8 @@ concat_binaries(X, Y) ->
 
 %% @doc Converts an integer to a list of specified length 
 %%      of BCD encoded bytes.
+%%
+%% @spec integer_to_bcd(integer(), integer()) -> list(byte())
 -spec(integer_to_bcd(integer(), integer()) -> list(byte())).
 
 integer_to_bcd(Value, Length) ->
@@ -109,6 +127,8 @@ integer_to_bcd(Value, Length) ->
 %% @doc Converts an ASCII hex encoded string to a list of BCD
 %%      encoded bytes padded with a specified padding character
 %%      if the string has odd length.
+%%
+%% @spec ascii_hex_to_bcd(string(), char()) -> list(byte())
 -spec(ascii_hex_to_bcd(string(), char()) -> list(byte())).
 
 ascii_hex_to_bcd(Value, PaddingChar) when length(Value) rem 2 =:= 1 ->
@@ -117,6 +137,8 @@ ascii_hex_to_bcd(Value, _PaddingChar) ->
 	ascii_hex_to_bcd2(Value, []).	
 	
 %% @doc Converts a list of BCD encoded bytes to an integer.
+%%
+%% @spec bcd_to_integer(list(byte())) -> integer()
 -spec(bcd_to_integer(list(byte())) -> integer()).
 
 bcd_to_integer(Bcd) ->
@@ -130,6 +152,8 @@ bcd_to_integer(Bcd) ->
 
 %% @doc Converts a BCD encoding of a value of specified length possibly
 %%      padded with a padding character to an ASCII hex string.
+%%
+%% @spec bcd_to_ascii_hex(list(binary()), integer(), char()) -> string()
 -spec(bcd_to_ascii_hex(list(binary()), integer(), char()) -> string()).
 
 bcd_to_ascii_hex(Bcd, Length, PaddingChar) when size(Bcd) =:= (Length + 1) div 2 ->
@@ -145,6 +169,8 @@ bcd_to_ascii_hex(Bcd, Length, PaddingChar) when size(Bcd) =:= (Length + 1) div 2
 
 %% @doc Converts a list of track 2 nibbles to a string containing
 %%      an ASCII encoding of the same data.
+%%
+%% @spec track2_to_string(list(byte()), integer()) -> string()
 -spec(track2_to_string(list(byte()), integer()) -> string()).
 
 track2_to_string(Data, Length) ->
@@ -152,6 +178,8 @@ track2_to_string(Data, Length) ->
 
 %% @doc Converts a string of ASCII characters to a track 2
 %%      encoding.
+%%
+%% @spec string_to_track2(string()) -> list(byte())
 -spec(string_to_track2(string()) -> list(byte())).
 
 string_to_track2(Data) ->
@@ -159,6 +187,8 @@ string_to_track2(Data) ->
 
 %% @doc Converts a string containing 1 ASCII hex character
 %%      to its value.
+%%
+%% @spec ascii_hex_to_digit(string()) -> integer()
 -spec(ascii_hex_to_digit(string()) -> integer()).
 
 ascii_hex_to_digit([A]) when A >= $0 andalso A =< $9 ->
@@ -170,6 +200,8 @@ ascii_hex_to_digit([A]) when A >= $a andalso A =< $f ->
 
 %% @doc Converts a value in the range 0-15 to a 1 character
 %%      string containing the equivalent hexadecimal digit.
+%%
+%% @spec digit_to_ascii_hex(integer()) -> string()
 -spec(digit_to_ascii_hex(integer()) -> string()).
 
 digit_to_ascii_hex(D) when D >= 0 andalso D =< 9 ->
