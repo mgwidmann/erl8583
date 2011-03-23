5e2855f677a3bdcff3b24cd0692abacbb7fcba34
hammingweight <hammingweight@cc4b8374-00f8-dcee-eb2c-e192c0053825>
2011-03-23 18:47:50 +0000

    Fixes to script.

--- trunk/make.sh
+++ trunk/make.sh
@@ -2,9 +2,14 @@ mkdir tbin
 rm ebin/*.beam
 rm tbin/*.beam
 rm doc/*
+echo "Compiling source"
 erlc -I include -o ebin src/*.erl
+echo "Compiling tests"
 erlc -I include -o tbin test/*.erl
 erl -noshell -pa ebin -pa tbin -s test_all test -s init stop
 ./gen_doc.sh
-zip dist/erl8583_0.1.0.zip src/* include/* doc/* ebin/* make.sh
+echo "Creating archive"
+mkdir dist
+rm dist/*.zip
+zip -q dist/erl8583_0.1.0.zip src/* test/* include/* doc/* ebin/* make.sh gen_doc.sh
 
