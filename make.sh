d4ff00817568e5de2dda1b687128755789d3b402
hammingweight <hammingweight@cc4b8374-00f8-dcee-eb2c-e192c0053825>
2011-03-23 17:56:36 +0000

    Moved tests to separate directory.

--- make.sh
+++ make.sh
@@ -1,6 +1,10 @@
+mkdir tbin
 rm ebin/*.beam
+rm tbin/*.beam
+rm doc/*
 erlc -I include -o ebin src/*.erl
-erl -noshell -pa ebin -s test_all test -s init stop
+erlc -I include -o tbin test/*.erl
+erl -noshell -pa ebin -pa tbin -s test_all test -s init stop
 ./gen_doc.sh
 zip dist/erl8583_0.1.0.zip src/* include/* doc/* ebin/* make.sh
 
