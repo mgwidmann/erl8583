003440c25b2672ad922650f83133de0a5c39bb44
hammingweight <hammingweight@cc4b8374-00f8-dcee-eb2c-e192c0053825>
2011-03-26 11:06:46 +0000

    Modified make script.

--- make.sh
+++ make.sh
@@ -1,15 +1,24 @@
 mkdir tbin
 rm ebin/*.beam
 rm tbin/*.beam
-rm doc/*.html
 echo "Compiling source"
 erlc -I include -o ebin src/*.erl
 echo "Compiling tests"
 erlc -I include -o tbin test/*.erl
 erl -noshell -pa ebin -pa tbin -s test_all test -s init stop
+rm -r erl8583
+mkdir erl8583
+mkdir erl8583/doc
+cp overview.edoc erl8583/doc
 ./gen_doc.sh
 echo "Creating archive"
-mkdir dist
-rm dist/*.zip
-zip -q dist/erl8583_0.1.0.zip src/* test/* include/* doc/* ebin/* make.sh gen_doc.sh
+rsync -p -r --exclude=".*" src erl8583
+rsync -p -r --exclude=".*" test erl8583
+rsync -p -r --exclude=".*" gen_doc.sh erl8583
+rsync -p -r --exclude=".*" include erl8583
+rsync -p -r --exclude=".*" ebin erl8583
+cp make.sh erl8583
+cp gen_doc.sh erl8583
+cp overview.edoc erl8583
+zip -q erl8583_0.1.0.zip -r erl8583
 
