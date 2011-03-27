6971a05fd897a88c4c55b73e45751a344f5fe60f
hammingweight <hammingweight@cc4b8374-00f8-dcee-eb2c-e192c0053825>
2011-03-27 17:46:11 +0000

    Refactoring.

--- src/erl8583_marshaller_ascii_bitmap.erl
+++ src/erl8583_marshaller_ascii_bitmap.erl
@@ -12,7 +12,7 @@
 %%
 %% Exported Functions
 %%
--export([marshal/1]).
+-export([marshal/1, unmarshal/1]).
 
 %%
 %% API Functions
@@ -26,6 +26,22 @@
 marshal(Message) ->
 	[0|Fields] = erl8583_message:get_fields(Message),
 	construct_bitmap(Fields).
+
+%% @doc Extracts a list of field IDs from an ASCII string 
+%%      representation of an ISO 8583 message. The result is returned
+%%      as a 2-tuple of the field IDs and the remainder of the 
+%%      the message (encoding the field values but not the bit map).
+%%
+%% @spec unmarshal(string()) -> {list(integer()), string()}
+-spec(unmarshal(string()) -> {list(integer()), string()}).
+
+unmarshal([]) ->
+	{[], []};
+unmarshal(AsciiMessage) ->
+	BitMapLength = get_bit_map_length(AsciiMessage),
+	{AsciiBitMap, Fields} = lists:split(BitMapLength, AsciiMessage),
+	BitMap = erl8583_convert:ascii_hex_to_string(AsciiBitMap),
+	extract_fields(BitMap, 0, 8, {[], Fields}).
 
 %%
 %% Local Functions
@@ -46,3 +62,31 @@ construct_bitmap([Field|Tail], Result) when Field > 0 ->
 	{Left, Right} = lists:split(ByteNum, Result),
 	[ToUpdate | RightRest] = Right,
 	construct_bitmap(Tail, Left ++ ([ToUpdate + (1 bsl BitNum)]) ++ RightRest).
+
+get_bit_map_length(Msg) ->
+	get_bit_map_length(Msg, 16).
+
+get_bit_map_length(Msg, Length) ->
+	[HexDig1, HexDig2|_Tail] = Msg,
+	<<Byte>> = erl8583_convert:ascii_hex_to_binary([HexDig1, HexDig2]),
+	case (Byte band 128) of
+		0 ->
+			Length;
+		_ ->
+			{_Msg1, Msg2} = lists:split(16, Msg),
+			get_bit_map_length(Msg2, Length+16)
+	end.
+
+extract_fields([], _Offset, _Index, {FieldIds, Fields}) ->
+	Ids = lists:sort(FieldIds),
+	{[Id || Id <- Ids, Id rem 64 =/= 1], Fields};
+extract_fields([_Head|Tail], Offset, 0, {FieldIds, Fields}) ->
+	extract_fields(Tail, Offset+1, 8, {FieldIds, Fields});
+extract_fields([Head|Tail], Offset, Index, {FieldIds, Fields}) ->
+	case Head band (1 bsl (Index-1)) of
+		0 ->
+			extract_fields([Head|Tail], Offset, Index-1, {FieldIds, Fields});
+		_ ->
+			extract_fields([Head|Tail], Offset, Index-1, {[Offset*8+9-Index|FieldIds], Fields})
+	end.
+			
