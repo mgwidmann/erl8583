a10c91453a143a3b51dc0763780d75edb4d6bfb7
hammingweight <hammingweight@cc4b8374-00f8-dcee-eb2c-e192c0053825>
2011-03-28 05:34:11 +0000

    Fix up comments.

--- src/erl8583_marshaller_ascii_bitmap.erl
+++ src/erl8583_marshaller_ascii_bitmap.erl
@@ -1,6 +1,20 @@
-%% Author: carl
-%% Created: 27 Mar 2011
-%% Description: TODO: Add description to erl8583_marshaller_ascii_bitmap
+% Licensed under the Apache License, Version 2.0 (the "License"); you may not
+% use this file except in compliance with the License. You may obtain a copy of
+% the License at
+%
+%   http://www.apache.org/licenses/LICENSE-2.0
+%
+% Unless required by applicable law or agreed to in writing, software
+% distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+% WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+% License for the specific language governing permissions and limitations under
+% the License.
+
+%% @author CA Meijer
+%% @copyright 2011 CA Meijer
+%% @doc This module constructs an ASCII string representation of the bit map of
+%%      an iso8583message() field.
+
 -module(erl8583_marshaller_ascii_bitmap).
 
 %%
@@ -17,6 +31,7 @@
 %%
 %% API Functions
 %%
+
 %% @doc Constructs an ASCII string representation of the
 %%      bitmap for an iso8583message().
 %%
