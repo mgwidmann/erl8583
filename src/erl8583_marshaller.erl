1cf1c745e04c4c62959019f2b9fc69ec92b2730d
hammingweight <hammingweight@cc4b8374-00f8-dcee-eb2c-e192c0053825>
2011-04-02 18:49:06 +0000

    Generic marshaller can marshal fields.

--- src/erl8583_marshaller.erl
+++ src/erl8583_marshaller.erl
@@ -23,12 +23,12 @@
 marshal(Message, Options) ->
 	OptionsRecord = parse_options(Options, #marshal_options{}),
 	FieldMarshalModule = OptionsRecord#marshal_options.field_marshaller,
-	EncodingRule = OptionsRecord#marshal_options.encoding_rules,
+	EncodingRules = OptionsRecord#marshal_options.encoding_rules,
 	if
 		FieldMarshalModule =:= undefined ->
 			Marshalled1 = [];
 		FieldMarshalModule =/= undefined ->
-			Mti = FieldMarshalModule:marshal_field(0, erl8583_message:get(0, Message), EncodingRule),
+			Mti = FieldMarshalModule:marshal_field(0, erl8583_message:get(0, Message), EncodingRules),
 			Marshalled1 = Mti
 	end,
 	BitmapMarshalModule = OptionsRecord#marshal_options.bitmap_marshaller,
@@ -40,7 +40,13 @@ marshal(Message, Options) ->
 			Bitmap = BitmapMarshalModule:marshal_bitmap(Fields),
 			Marshalled2 = Marshalled1 ++ Bitmap
 	end,
-	Marshalled2.
+	if
+		FieldMarshalModule =:= undefined ->
+			Marshalled3 = Marshalled2;
+		FieldMarshalModule =/= undefined ->
+			Marshalled3 = Marshalled2 ++ encode(Fields, Message, FieldMarshalModule, EncodingRules) 
+	end,
+	Marshalled3.
 
 
 %%
@@ -52,3 +58,13 @@ parse_options([{field_marshaller, Marshaller}|Tail], OptionsRecord) ->
 	parse_options(Tail, OptionsRecord#marshal_options{field_marshaller=Marshaller});
 parse_options([{bitmap_marshaller, Marshaller}|Tail], OptionsRecord) ->
 	parse_options(Tail, OptionsRecord#marshal_options{bitmap_marshaller=Marshaller}).
+
+encode(Fields, Msg, FieldMarshaller, EncodingRules) ->
+	encode(Fields, Msg, [], FieldMarshaller, EncodingRules).
+
+encode([], _Msg, Result, _FieldMarshaller, _EncodingRules) ->
+	lists:reverse(Result);
+encode([FieldId|Tail], Msg, Result, FieldMarshaller, EncodingRules) ->
+	Value = erl8583_message:get(FieldId, Msg),
+	EncodedValue = FieldMarshaller:marshal_field(FieldId, Value, EncodingRules),
+	encode(Tail, Msg, lists:reverse(EncodedValue) ++ Result, FieldMarshaller, EncodingRules).
