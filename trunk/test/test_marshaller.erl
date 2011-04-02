c18537f5fed89d25d7e3b56221b3c213cbda3792
hammingweight <hammingweight@cc4b8374-00f8-dcee-eb2c-e192c0053825>
2011-04-02 18:19:28 +0000

    Generic marshaller marshals bitmaps.

--- trunk/test/test_marshaller.erl
+++ trunk/test/test_marshaller.erl
@@ -11,7 +11,7 @@
 %%
 %% Exported Functions
 %%
--export([marshal_field/3]).
+-export([marshal_field/3, marshal_bitmap/1]).
 
 %%
 %% API Functions
@@ -19,11 +19,21 @@
 marshal_field(0, "0200", erl8583_fields) ->
 	[0,2,0,0].
 
+marshal_bitmap([1, 2, 3]) ->
+	"bitmap = 123".
+
 mti_test() ->
 	Message = erl8583_message:set(0, "0200", erl8583_message:new()),
 	[0, 2, 0, 0] = erl8583_marshaller:marshal(Message, [{field_marshaller, ?MODULE}]).
 
+bitmap_test() ->
+	Message0 = erl8583_message:set(0, "0200", erl8583_message:new()),
+	Message1 = erl8583_message:set(1, "1", Message0),
+	Message2 = erl8583_message:set(2, "1", Message1),	
+	Message3 = erl8583_message:set(3, "1", Message2),
+	"bitmap = 123" = erl8583_marshaller:marshal(Message3, [{bitmap_marshaller, ?MODULE}]).
 
+	
 %%
 %% Local Functions
 %%
