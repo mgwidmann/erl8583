435f900c1155814319173cecf4fbea13106d5875
hammingweight <hammingweight@cc4b8374-00f8-dcee-eb2c-e192c0053825>
2011-04-02 18:19:28 +0000

    Generic marshaller marshals bitmaps.

--- src/erl8583_marshaller.erl
+++ src/erl8583_marshaller.erl
@@ -8,6 +8,11 @@
 %%
 
 %%
+%% Records
+%%
+-record(marshal_options, {field_marshaller, bitmap_marshaller, encoding_rules=erl8583_fields}).
+
+%%
 %% Exported Functions
 %%
 -export([marshal/2]).
@@ -16,13 +21,34 @@
 %% API Functions
 %%
 marshal(Message, Options) ->
-	[{field_marshaller, Module}] = Options,
-	Module:marshal_field(0, erl8583_message:get(0, Message), erl8583_fields).
-
-
+	OptionsRecord = parse_options(Options, #marshal_options{}),
+	FieldMarshalModule = OptionsRecord#marshal_options.field_marshaller,
+	EncodingRule = OptionsRecord#marshal_options.encoding_rules,
+	if
+		FieldMarshalModule =:= undefined ->
+			Marshalled1 = [];
+		FieldMarshalModule =/= undefined ->
+			Mti = FieldMarshalModule:marshal_field(0, erl8583_message:get(0, Message), EncodingRule),
+			Marshalled1 = Mti
+	end,
+	BitmapMarshalModule = OptionsRecord#marshal_options.bitmap_marshaller,
+	[0|Fields] = erl8583_message:get_fields(Message),
+	if
+		BitmapMarshalModule =:= undefined ->
+			Marshalled2 = Marshalled1;
+		BitmapMarshalModule =/= undefined ->			
+			Bitmap = BitmapMarshalModule:marshal_bitmap(Fields),
+			Marshalled2 = Marshalled1 ++ Bitmap
+	end,
+	Marshalled2.
 
 
 %%
 %% Local Functions
 %%
-
+parse_options([], OptionsRecord) ->
+	OptionsRecord;
+parse_options([{field_marshaller, Marshaller}|Tail], OptionsRecord) ->
+	parse_options(Tail, OptionsRecord#marshal_options{field_marshaller=Marshaller});
+parse_options([{bitmap_marshaller, Marshaller}|Tail], OptionsRecord) ->
+	parse_options(Tail, OptionsRecord#marshal_options{bitmap_marshaller=Marshaller}).
