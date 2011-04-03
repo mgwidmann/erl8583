0cec0c5fb4e83837873e1e2a5224bfdad8616d8f
hammingweight <hammingweight@cc4b8374-00f8-dcee-eb2c-e192c0053825>
2011-04-03 14:24:25 +0000

    Generic marshaller can unmarshal bitmap and fields.

--- src/erl8583_marshaller.erl
+++ src/erl8583_marshaller.erl
@@ -18,7 +18,7 @@
 %%
 %% Exported Functions
 %%
--export([marshal/2]).
+-export([marshal/2, unmarshal/2]).
 
 %%
 %% API Functions
@@ -30,6 +30,12 @@ marshal(Message, Options) ->
 	Marshalled3 = Marshalled2 ++ encode_fields(OptionsRecord, Message),
 	wrap_message(OptionsRecord, Message, Marshalled3).
 
+unmarshal(Marshalled, Options) ->
+	OptionsRecord = parse_options(Options, #marshal_options{}),
+	Marshalled1 = Marshalled,
+	{Message, Marshalled2} = decode_mti(OptionsRecord, Marshalled1, erl8583_message:new()),
+	{FieldIds, Marshalled3} = decode_bitmap(OptionsRecord, Marshalled2),
+	decode_fields(FieldIds, Message, OptionsRecord, Marshalled3).
 
 %%
 %% Local Functions
@@ -71,6 +77,24 @@ encode_mti(Options, Message) ->
 		FieldMarshalModule =/= undefined ->
 			FieldMarshalModule:marshal_field(0, erl8583_message:get(0, Message), EncodingRules)
 	end.
+
+decode_mti(Options, Marshalled, Message) ->
+	Encoding = Options#marshal_options.encoding_rules,
+	case Encoding of
+		undefined ->
+			EncodingRules = erl8583_fields;
+		_ ->
+			EncodingRules = Encoding
+	end,
+	FieldMarshalModule = Options#marshal_options.field_marshaller,
+	if
+		FieldMarshalModule =:= undefined ->
+			{Message, Marshalled};
+		FieldMarshalModule =/= undefined ->
+			{FieldValue, Rest} = FieldMarshalModule:unmarshal_field(0, Marshalled, EncodingRules),
+			{erl8583_message:set(0, FieldValue, Message), Rest}
+	end.
+	
 	
 encode_bitmap(Options, Message) ->
 	BitmapMarshalModule = Options#marshal_options.bitmap_marshaller,
@@ -80,6 +104,15 @@ encode_bitmap(Options, Message) ->
 			[];
 		BitmapMarshalModule =/= undefined ->			
 			BitmapMarshalModule:marshal_bitmap(Fields)
+	end.
+
+decode_bitmap(Options, Marshalled) ->
+	BitmapMarshalModule = Options#marshal_options.bitmap_marshaller,
+	if
+		BitmapMarshalModule =:= undefined ->
+			{[], Marshalled};
+		BitmapMarshalModule =/= undefined ->			
+		BitmapMarshalModule:unmarshal_bitmap(Marshalled)
 	end.
 
 encode_fields(Options, Message) ->
@@ -102,6 +135,25 @@ encode([FieldId|Tail], Msg, Result, FieldMarshaller, EncodingRules) ->
 	Value = erl8583_message:get(FieldId, Msg),
 	EncodedValue = FieldMarshaller:marshal_field(FieldId, Value, EncodingRules),
 	encode(Tail, Msg, lists:reverse(EncodedValue) ++ Result, FieldMarshaller, EncodingRules).
+
+%decode_fields(FieldIds, Message, OptionsRecord, Marshalled) ->
+%	Message.
+decode_fields([], Result, _OptionsRecord, _Marshalled) ->
+	Result;
+decode_fields([FieldId|Tail], Message, Options, Marshalled) ->
+	EncodingRules = get_encoding_rules(Options, Message),
+	FieldMarshalModule = Options#marshal_options.field_marshaller,
+	if
+		FieldMarshalModule =:= undefined ->
+			Message;
+		FieldMarshalModule =/= undefined ->
+			{FieldValue, Rest} = FieldMarshalModule:unmarshal_field(FieldId, Marshalled, EncodingRules),
+			decode_fields(Tail, erl8583_message:set(FieldId, FieldValue, Message), 
+						  Options, Rest) 
+	end.
+	%{Value, UpdatedFields} = FieldMarshaller:unmarshal_field(FieldId, Fields),
+	%UpdatedResult = erl8583_message:set(FieldId, Value, Result),
+	%decode_fields(Tail, UpdatedFields, UpdatedResult, FieldMarshaller).
 
 wrap_message(Options, Message, Marshalled) ->
 	WrapperMarshalModule = Options#marshal_options.wrapping_marshaller,
