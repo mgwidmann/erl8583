a2ab12f529af031313920b51be757b6b56eef06c
hammingweight <hammingweight@cc4b8374-00f8-dcee-eb2c-e192c0053825>
2011-04-04 18:14:40 +0000

    Removed erl8583_marshaller_binary_bitmap.

--- src/erl8583_marshaller_binary.erl
+++ src/erl8583_marshaller_binary.erl
@@ -28,6 +28,7 @@
 %% Exported Functions
 %%
 -export([marshal/1, marshal/2, marshal/3, unmarshal/1, unmarshal/2, unmarshal/3]).
+-export([marshal_bitmap/1, unmarshal_bitmap/1]).
 
 %%
 %% API Functions
@@ -50,7 +51,7 @@ marshal(Message) ->
 -spec(marshal(iso8583message(), module()) -> binary()).
 
 marshal(Message, FieldMarshaller) ->
-	marshal(Message, FieldMarshaller, erl8583_marshaller_binary_bitmap).
+	marshal(Message, FieldMarshaller, erl8583_marshaller_binary).
 
 %% @doc Marshals an ISO 8583 message into an ASCII string. This function
 %%      uses the specified field marshalling module and the 
@@ -81,7 +82,7 @@ unmarshal(BinaryMessage) ->
 -spec(unmarshal(binary(), module()) -> iso8583message()).
 
 unmarshal(BinaryMessage, FieldMarshaller) ->
-	unmarshal(BinaryMessage, FieldMarshaller, erl8583_marshaller_binary_bitmap).
+	unmarshal(BinaryMessage, FieldMarshaller, erl8583_marshaller_binary).
 
 %% @doc Unmarshals a binary into an ISO 8583 message. This function uses
 %%      the specified field marshalling module and the specified
@@ -94,6 +95,65 @@ unmarshal(BinaryMessage, FieldMarshaller, BitMapMarshaller) ->
 	erl8583_marshaller:unmarshal(BinaryMessage, [{field_marshaller, FieldMarshaller},
 												 {bitmap_marshaller, BitMapMarshaller}]).
 
+%% @doc Constructs a binary representation of the
+%%      bitmap for an iso8583message().
+%%
+%% @spec marshal_bitmap(list(integer())) -> list(byte())
+-spec(marshal_bitmap(list(integer())) -> list(byte())).
+
+marshal_bitmap([]) ->
+	[];
+marshal_bitmap(FieldIds) ->
+	NumBitMaps = (lists:max(FieldIds) + 63) div 64,
+	ExtensionBits = [Bit * 64 - 127 || Bit <- lists:seq(2, NumBitMaps)],
+	BitMap = lists:duplicate(NumBitMaps * 8, 0),
+	construct_bitmap(lists:sort(ExtensionBits ++ FieldIds), BitMap).
+
+%% @doc Extracts a list of field IDs from a binary representation of 
+%%      an ISO 8583 message.  The result is returned as a 2-tuple: a list
+%%      of field IDs and the remainder of the message excluding the bit map.
+%%
+%% @spec unmarshal_bitmap(list(byte())) -> {list(integer()), list(byte())}
+-spec(unmarshal_bitmap(list(byte())) -> {list(integer()), list(byte())}).
+
+unmarshal_bitmap([]) ->
+	{[], []};
+unmarshal_bitmap(BinaryMessage) ->
+	BitMapLength = get_bit_map_length(BinaryMessage),
+	{BitMap, Fields} = lists:split(BitMapLength, BinaryMessage),
+	{extract_fields(BitMap, 0, 8, []), Fields}.
+
 %%
 %% Local Functions
 %%
+construct_bitmap([], Result) ->
+	Result;
+construct_bitmap([Field|Tail], Result) when Field > 0 ->
+	ByteNum = (Field - 1) div 8,
+	BitNum = 7 - ((Field - 1) rem 8),
+	{Left, Right} = lists:split(ByteNum, Result),
+	[ToUpdate | RightRest] = Right,
+	construct_bitmap(Tail, Left ++ ([ToUpdate + (1 bsl BitNum)]) ++ RightRest).
+
+get_bit_map_length(Message) ->
+	[Head|_Tail] = Message,
+	case Head >= 128 of
+		false ->
+			8;
+		true ->
+			{_, Rest} = lists:split(8, Message),
+			8 + get_bit_map_length(Rest)
+	end.
+
+extract_fields([], _Offset, _Index, FieldIds) ->
+	Ids = lists:sort(FieldIds),
+	[Id || Id <- Ids, Id rem 64 =/= 1];
+extract_fields([_Head|Tail], Offset, 0, FieldIds) ->
+	extract_fields(Tail, Offset+1, 8, FieldIds);
+extract_fields([Head|Tail], Offset, Index, FieldIds) ->
+	case Head band (1 bsl (Index-1)) of
+		0 ->
+			extract_fields([Head|Tail], Offset, Index-1, FieldIds);
+		_ ->
+			extract_fields([Head|Tail], Offset, Index-1, [Offset*8+9-Index|FieldIds])
+	end.
