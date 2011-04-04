440be9f7e2beee33e621d6eb4d4971c8a8f1ecd5
hammingweight <hammingweight@cc4b8374-00f8-dcee-eb2c-e192c0053825>
2011-04-04 17:05:16 +0000

    Removed untested functions from EBCDIC marshaller.

--- src/erl8583_marshaller_ebcdic_field.erl
+++ src/erl8583_marshaller_ebcdic_field.erl
@@ -27,21 +27,11 @@
 %%
 %% Exported Functions
 %%
--export([marshal_field/2, unmarshal_field/2, unmarshal_data_element/2]).
+-export([marshal_field/2]).
 
 %%
 %% API Functions
 %%
-
-%% @doc Extracts a field value from the start of an EBCDIC binary given how the field
-%%      is encoded.  The field value and the rest of the unmarshalled EBCDIC binary
-%%      is returned as a 2-tuple.
-%%
-%% @spec unmarshal_data_element(Encoding::field_encoding(), binary()) -> {iso8583field_value(), binary()}
--spec(unmarshal_data_element(field_encoding(), binary()) -> {iso8583field_value(), binary()}).
-
-unmarshal_data_element(_,_) ->
-	{<<>>, <<>>}.
 
 %% @doc Marshals a field value into an EBCDIC binary. The 1987 version
 %%      of the ISO 8583 specification is used to determine how to
@@ -53,18 +43,6 @@ unmarshal_data_element(_,_) ->
 marshal_field(FieldId, FieldValue) ->
 	AsciiEncoding = erl8583_marshaller_ascii_field:marshal_field(FieldId, FieldValue, erl8583_fields),
 	erl8583_convert:ascii_to_ebcdic(AsciiEncoding).
-
-%% @doc Extracts a field value from the start of an EBCDIC binary.  The field value 
-%%      and the rest of the unmarshalled EBCDIC binary are returned as a 2-tuple.
-%%      The 1987 version of the ISO 8583 specification is used to determine how to
-%%      decode the field value.
-%%
-%% @spec unmarshal_field(integer(), binary()) -> {iso8583field_value(), binary()}
--spec(unmarshal_field(integer(), binary()) -> {iso8583field_value(), binary()}).
-
-unmarshal_field(_, _) ->
-	{<<>>, <<>>}.
-
 
 %%
 %% Local Functions
