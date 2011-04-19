a4eef3208a2758ed00fecdb422d950e6cd831c9e
hammingweight <hammingweight@cc4b8374-00f8-dcee-eb2c-e192c0053825>
2011-04-19 00:35:15 +0000

    Bug fix: convert bitmap to list returned elements in reverse order.

--- test/test_convert.erl
+++ test/test_convert.erl
@@ -211,4 +211,5 @@ list_to_bitmap_test() ->
 	<<128, 0, 0, 0, 0, 0, 0, 1>> = erl8583_convert:list_to_bitmap([1, 1, 64, 64, 1]).
 
 bitmap_to_list_test() ->
-	[1] = erl8583_convert:bitmap_to_list(<<128, 0, 0, 0, 0, 0, 0, 0>>).
+	[1] = erl8583_convert:bitmap_to_list(<<128, 0, 0, 0, 0, 0, 0, 0>>),
+	[1, 2, 64] = erl8583_convert:bitmap_to_list(<<192, 0, 0, 0, 0, 0, 0, 1>>).
