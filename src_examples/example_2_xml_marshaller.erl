6af74bc60c544d3d296405c032237bcd518494c0
hammingweight <hammingweight@cc4b8374-00f8-dcee-eb2c-e192c0053825>
2011-04-26 18:51:15 +0000

    Some more examples written.

--- src_examples/example_2_xml_marshaller.erl
+++ src_examples/example_2_xml_marshaller.erl
@@ -1,6 +1,7 @@
-%% An example that demonstrates setting and getting data elements in an
-%% iso8583message().
--module(example_1_message).
+%% Author: carl
+%% Created: 26 Apr 2011
+%% Description: TODO: Add description to example_2_xml_marshaller
+-module(example_2_xml_marshaller).
 
 %%
 %% Include files
@@ -24,10 +25,4 @@ test() ->
 	Msg3 = erl8583_message:set(?CARD_ACCEPTOR_NAME_LOCATION, "ZIB Head Office ATM    V/I Lagos    01NG", Msg2),
 	Msg4 = erl8583_message:set(?MESSAGE_AUTHENTICATION_CODE, <<1,2,3,4,5,6,7,8>>, Msg3),
 	
-	% Display the fields defined for the message.
-	io:format("Fields: ~w~n", [erl8583_message:get_fields(Msg4)]),
-	
-	% Display fields 0, 43 and 64.
-	io:format("MTI:                         ~s~n", [erl8583_message:get(0, Msg4)]),
-	io:format("Card acceptor name/location: ~s~n", [erl8583_message:get(43, Msg4)]),
-	io:format("MAC:                         ~p~n", [erl8583_message:get(64, Msg4)]).
+	io:format("Marshalled:~n~p~n", [erl8583_marshaller_xml:marshal(Msg4)]).
