094c671262ea3b792cb4fee53af417224fdd6dc1
hammingweight <hammingweight@cc4b8374-00f8-dcee-eb2c-e192c0053825>
2011-04-26 13:09:44 +0000

    Expanded first example.

--- trunk/src_examples/message_example.erl
+++ trunk/src_examples/message_example.erl
@@ -20,10 +20,15 @@ test() ->
 	% Create a message.
 	Msg1 = erl8583_message:new(),
 	
-	% Set the MTI (field 0) and a MAC (field 64).
+	% Set the MTI (field 0), PAN (field 2) and a MAC (field 64).
 	Msg2 = erl8583_message:set_mti("0200", Msg1),
-	Msg3 = erl8583_message:set(?MESSAGE_AUTHENTICATION_CODE, <<1,2,3,4,5,6,7,8>>, Msg2),
+	Msg3 = erl8583_message:set(?PAN, "1234567890123456", Msg2),
+	Msg4 = erl8583_message:set(?MESSAGE_AUTHENTICATION_CODE, <<1,2,3,4,5,6,7,8>>, Msg3),
 	
-	% Display the MTI and MAC.
-	io:format("MTI: ~p~n", [erl8583_message:get(0, Msg3)]),
-	io:format("MAC: ~p~n", [erl8583_message:get(64, Msg3)]).
+	% Display the fields defined for the message.
+	io:format("Fields: ~p~n", [erl8583_message:get_fields(Msg4)]),
+	
+	% Display the MTI, PAN and MAC.
+	io:format("MTI:    ~p~n", [erl8583_message:get(0, Msg4)]),
+	io:format("PAN:    ~p~n", [erl8583_message:get(2, Msg4)]),
+	io:format("MAC:    ~p~n", [erl8583_message:get(64, Msg4)]).
