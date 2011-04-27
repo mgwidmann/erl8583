70d80ee920f810edf2fcd9fa637f2e820bc794dd
hammingweight <hammingweight@cc4b8374-00f8-dcee-eb2c-e192c0053825>
2011-04-27 13:34:28 +0000

    Expanded example 2 and added it to the user guide.

--- src_examples/example_1_message.erl
+++ src_examples/example_1_message.erl
@@ -27,20 +27,20 @@ test() ->
 	Msg2 = erl8583_message:set_mti("0200", Msg1),
 	Msg3 = erl8583_message:set(?CARD_ACCEPTOR_NAME_LOCATION, "ZIB Head Office ATM    V/I Lagos    01NG", Msg2),
 	Msg4 = erl8583_message:set(?MESSAGE_AUTHENTICATION_CODE, <<1,2,3,4,5,6,7,8>>, Msg3),
-	% Field 127 is a message containing subfields 127.2 and 127.3.
+	% Field 127 is a message containing subfields 127.2 and 127.12.
 	% Notice that we use two different ways of setting the value of a subfield.
-	% The way we set subfield 127.3 is the preferred idiom.
+	% The way we set subfield 127.12 is the preferred idiom.
 	SubMsg1 = erl8583_message:new(),
 	SubMsg2 = erl8583_message:set(2, "0000387020", SubMsg1),
 	Msg5 = erl8583_message:set(127, SubMsg2, Msg4),
-	Msg6 = erl8583_message:set([127, 3], "ZIBeTranzSnk", Msg5),
+	Msg6 = erl8583_message:set([127, 12], "ZIBeTranzSnk", Msg5),
 	
 	% Display the fields defined for the message.
 	io:format("Fields: ~w~n", [erl8583_message:get_fields(Msg6)]),
 	
-	% Display fields 0, 43, 64, 127.2 and 127.3.
+	% Display fields 0, 43, 64, 127.2 and 127.12.
 	io:format("MTI:                         ~s~n", [erl8583_message:get(0, Msg6)]),
 	io:format("Card acceptor name/location: ~s~n", [erl8583_message:get(43, Msg6)]),
 	io:format("MAC:                         ~p~n", [erl8583_message:get(64, Msg6)]),
 	io:format("Field 127.2:                 ~s~n", [erl8583_message:get([127, 2], Msg6)]),
-	io:format("Field 127.3:                 ~s~n", [erl8583_message:get([127, 3], Msg6)]).
+	io:format("Field 127.12:                ~s~n", [erl8583_message:get([127, 12], Msg6)]).
