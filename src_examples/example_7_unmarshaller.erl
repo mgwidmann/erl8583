20fdf3faeb6015b1cb4a282cb2f22d4645391fa5
hammingweight <hammingweight@cc4b8374-00f8-dcee-eb2c-e192c0053825>
2011-04-29 09:53:48 +0000

    Added comments for example 7 code.

--- src_examples/example_7_unmarshaller.erl
+++ src_examples/example_7_unmarshaller.erl
@@ -19,16 +19,25 @@ unmarshal_init(Message, Marshalled) ->
 %
 % Special handling for field 127.
 % The length of field 127 is encoded in 6 bytes rather than 3.
-% After extracting the value of field 127 we unmarshal it.
+% After extracting the value of field 127 we unmarshal it since
+% it contains subfields.
 unmarshal_field(127, Marshalled, _EncodingRules) ->
 	{LenStr, Rest} = lists:split(6, Marshalled),
 	Len = list_to_integer(LenStr),
 	{Value, MarshalledRem} = lists:split(Len, Rest),
+	
+	% Notice:
+	% 1. That we need to specify how the subfields of field 127 are
+	%    encoded.
+	% 2. We don't specify a marshaller for the MTI (since there isn't
+	%    a message type).
 	MarshallingOptions = [{bitmap_marshaller, erl8583_marshaller_binary},
 						  {field_marshaller, ?MODULE},
-						  {encoding_rules, example_7_encoding_rules}],
-	Message = erl8583_marshaller:unmarshal(Value, MarshallingOptions),
-	{Message, MarshalledRem, []};
+						  {encoding_rules, example_7_field127_rules}],
+	Message127 = erl8583_marshaller:unmarshal(Value, MarshallingOptions),
+	
+	% Return the unmarshalled message as the value of field 127.
+	{Message127, MarshalledRem, []};
 
 % Use the binary marshaller to unmarshal binary fields and
 % the ASCII marshaller to unmarshal all other fields.
