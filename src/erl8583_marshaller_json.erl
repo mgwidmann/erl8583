2b1449686cdb2567d437c7f8e6bf96a0193b3aa5
hammingweight <hammingweight@cc4b8374-00f8-dcee-eb2c-e192c0053825>
2011-11-06 13:22:06 +0000

    Started work on a JSON marshaller.

---
+++ src/erl8583_marshaller_json.erl
@@ -0,0 +1,64 @@
+% Licensed under the Apache License, Version 2.0 (the "License"); you may not
+% use this file except in compliance with the License. You may obtain a copy of
+% the License at
+%
+%   http://www.apache.org/licenses/LICENSE-2.0
+%
+% Unless required by applicable law or agreed to in writing, software
+% distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+% WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+% License for the specific language governing permissions and limitations under
+% the License.
+
+%% @author CA Meijer
+%% @copyright 2011 CA Meijer
+%% @doc This module marshals an iso8583message() 
+%%      into a JSON document and can unmarshal a JSIBL document into
+%%      an iso8583message(). This module also exposes
+%%      functions for explicitly marshalling/unmarshalling
+%%      the MTI, bitmap and fields of a message. This is
+%%      to conform to the design of other marshallers and
+%%      the contract required by the erl8583_marshaller.
+-module(erl8583_marshaller_json).
+
+%%
+%% Include files
+%%
+%% @headerfile "../include/erl8583_types.hrl"
+-include("erl8583_types.hrl").
+-include("erl8583_marshallers.hrl").
+
+%%
+%% Exported Functions
+%%
+-export([marshal/1,
+		 unmarshal/1]).
+
+%%
+%% API Functions
+%%
+
+%% @doc Constructs a JSON document of
+%%      an iso8583message().
+%%
+%% @spec marshal(iso8583message()) -> string()
+-spec(marshal(iso8583message()) -> string()).
+
+marshal(Message) ->
+	erl8583_marshaller:marshal(Message, ?MARSHALLER_JSON).
+
+%% @doc Constructs an iso8583message() from a JSON document 
+%%      representation of the message.
+%%
+%% @spec unmarshal(string()) -> iso8583message()
+-spec(unmarshal(string()) -> iso8583message()).
+
+unmarshal(Marshalled) ->
+	erl8583_marshaller:unmarshal(Marshalled, ?MARSHALLER_JSON).
+
+
+
+%%
+%% Local Functions
+%%
+
