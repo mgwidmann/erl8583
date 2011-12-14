53e5fa14dcd264d5536c54d2e798c9880b4670ee
hammingweight <hammingweight@cc4b8374-00f8-dcee-eb2c-e192c0053825>
2011-12-14 05:26:03 +0000

    Added a module to move functions to that don't fit neatly into the erl8583_message_module.

---
+++ src_examples/erl8583_message_helpers.erl
@@ -0,0 +1,40 @@
+% Licensed under the Apache License, Version 2.0 (the "License"); you may not
+% use this file except in compliance with the License. You may obtain a copy of
+% the License at
+%
+%   http://www.apache.org/licenses/LICENSE-2.0
+%
+% Unless required by applicable law or agreed to in writing, software
+% distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+% WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+% License for the specific language governing permissions and limitations under
+% the License.
+
+%% @author CA Meijer
+%% @copyright 2011 CA Meijer
+%% @doc Functions for manipulating erl8583_messages such as changing the
+%%      message type to indicate that a message is a repeat. Functions
+%%      exist to construct a new message from an old one, retaining a 
+%%      subset of the message fields which can be useful when constructing
+%%      a response message.
+-module(erl8583_message_helpers).
+
+%%
+%% Include files
+%%
+
+%%
+%% Exported Functions
+%%
+-export([]).
+
+%%
+%% API Functions
+%%
+
+
+
+%%
+%% Local Functions
+%%
+
