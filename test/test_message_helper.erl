d1c3fa71299162a32167560f6f3a70791d6f585a
hammingweight <hammingweight@cc4b8374-00f8-dcee-eb2c-e192c0053825>
2011-12-15 05:37:23 +0000

    Function bloat. Remove response function from erl8583_message module.

--- test/test_message_helper.erl
+++ test/test_message_helper.erl
@@ -34,3 +34,45 @@ repeat_test() ->
 	Msg4Rep = erl8583_message_helpers:repeat(Msg4),
 	"0105" = erl8583_message:get(0, Msg4Rep).
 
+response_1_test() ->
+	Message = erl8583_message:new(),
+	Message2 = erl8583_message:set(10, "hello", Message),
+	Message3 = erl8583_message:set(0, "0200", Message2),
+	Message4 = erl8583_message:set(2, "hello2", Message3),
+	Response1 = erl8583_message_helpers:response([0, 10], Message4),
+	[0, 10] = erl8583_message:get_fields(Response1),
+	"hello" = erl8583_message:get(10, Response1),
+	"0210" = erl8583_message:get(0, Response1),
+	Response2 = erl8583_message_helpers:response([2, 10], Message4),
+	[0, 2, 10] = erl8583_message:get_fields(Response2),
+	"hello2" = erl8583_message:get(2, Response2),
+	"0210" = erl8583_message:get(0, Response2).
+	
+response_2_test() ->
+	Message = erl8583_message:new(),
+	Message2 = erl8583_message:set(10, "hello", Message),
+	Message3 = erl8583_message:set(0, "0220", Message2),
+	Message4 = erl8583_message:set(2, "hello2", Message3),
+	Response = erl8583_message_helpers:response(Message4),
+	[0, 2, 10] = erl8583_message:get_fields(Response),
+	"hello2" = erl8583_message:get(2, Response),
+	"0230" = erl8583_message:get(0, Response).
+
+% response shouldn't use the keep the repeat digit.
+response_3_test() ->
+	Message = erl8583_message:new(),
+	Message2 = erl8583_message:set(10, "hello", Message),
+	Message3 = erl8583_message:set(0, "0221", Message2),
+	Response = erl8583_message_helpers:response(Message3),
+	[0, 10] = erl8583_message:get_fields(Response),
+	"hello" = erl8583_message:get(10, Response),
+	"0230" = erl8583_message:get(0, Response).
+	
+% response shouldn't use the keep the repeat digit.
+response_4_test() ->
+	Message = erl8583_message:new(),
+	Message2 = erl8583_message:set(0, "0423", Message),
+	Response = erl8583_message_helpers:response(Message2),
+	[0] = erl8583_message:get_fields(Response),
+	"0432" = erl8583_message:get(0, Response).
+	
